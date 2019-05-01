aa7b65b37d73116f8de0291ca4f385851de06e51
Androz2091 <androz2091@gmail.com>
2019-05-01 21:15:29 +0200

    Auto creation of the right invitation link

--- commands/General/invite.js
+++ commands/General/invite.js
@@ -22,18 +22,18 @@ class Invite extends Command {
 
     async run (message, args, membersdata, guild_data, data) {
 
-        if(args[0]) return message.channel.send('https://discordapp.com/oauth2/authorize?client_id=563420709423153152&scope=bot&permissions=2146958847');
+        if(args[0]) return message.channel.send(`https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2146958847`);
 
         var embed = new Discord.RichEmbed()
             .setAuthor(message.language.get('INVITE_HEADING'))
             .setDescription(message.language.get('INVITE_DESC', guild_data.prefix))
-            .addField(message.language.get('INVITE_FIELD1'), 'https://discordapp.com/oauth2/authorize?client_id=563420709423153152&scope=bot&permissions=2146958847')
+            .addField(message.language.get('INVITE_FIELD1'), `https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2146958847`)
             .setColor(data.embed.color)
             .setFooter(data.embed.footer)
             
         this.client.functions.supportLink(this.client).then(url => {
             embed.addField(message.language.get('INVITE_FIELD3'), url)
-            .addField(message.language.get('INVITE_FIELD2'), 'https://discordbots.org/bot/557445719892688897/vote')
+            .addField(message.language.get('INVITE_FIELD2'), `https://discordbots.org/bot/${this.client.user.id}/vote`)
             message.channel.send(embed);
         });
            
