6c8fc65ece74c7c26de7d854f628f7549b35bd46
Ernest <47856136+Ernest05@users.noreply.github.com>
2019-05-03 09:05:31 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,51 +1,49 @@
 /*
 Logger class for easy and aesthetically pleasing console logging
 */
-const chalk = require("chalk");
+const { bgBlue, bdRed, black, green } = require("chalk");
 const moment = require("moment");
 
-class Logger {
+module.exports = class Logger {
   static log (content, type = "log") {
     const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
     switch (type) {
       // Check the message type and then print him in the console
       case "log": {
-        return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
+        return console.log(`${timestamp} ${bgBlue(type.toUpperCase())} ${content} `);
       }
       case "warn": {
-        return console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
+        return console.log(`${timestamp} ${black.bgYellow(type.toUpperCase())} ${content} `);
       }
       case "error": {
-        return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
+        return console.log(`${timestamp} ${bgRed(type.toUpperCase())} ${content} `);
       }
       case "debug": {
-        return console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
+        return console.log(`${timestamp} ${green(type.toUpperCase())} ${content} `);
       }
       case "cmd": {
-        return console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
+        return console.log(`${timestamp} ${black.bgWhite(type.toUpperCase())} ${content}`);
       }
       case "ready": {
-        return console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
+        return console.log(`${timestamp} ${black.bgGreen(type.toUpperCase())} ${content}`);
       } 
       default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
-    } 
-  }
+    };
+  };
   
   static error (content) {
     return this.log(content, "error");
-  }
+  };
   
   static warn (content) {
     return this.log(content, "warn");
-  }
+  };
   
   static debug (content) {
     return this.log(content, "debug");
-  } 
+  };
   
   static cmd (content) {
     return this.log(content, "cmd");
-  }
-}
-
-module.exports = Logger;
+  };
+};
