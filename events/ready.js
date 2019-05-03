dadda589b9ab8f8acbeb4b6a2f6636f73f1373c5
Androz2091 <androz2091@gmail.com>
2019-05-03 10:43:51 +0200

    Disable clientUser#fetchApplication() and update games

--- events/ready.js
+++ events/ready.js
@@ -8,29 +8,30 @@ module.exports = class {
 
     async run () {
 
-        // Wait one second, if the bot is not really ready
-        await this.client.wait(1000);
-
-        // Fetch discord app
-        this.client.appInfo = await this.client.fetchApplication();
-        setInterval( async () => {
-            this.client.appInfo = await this.client.fetchApplication();
-        }, 60000);
-
         // Logs some informations using the logger file
         this.client.logger.log(`Loading a total of ${this.client.commands.size} command(s).`, 'log');
         this.client.logger.log(`${this.client.user.tag}, ready to serve ${this.client.users.size} users in ${this.client.guilds.size} servers.`, "ready");
+        
 
         // Update the game every 20s
         var games = [
-            `${this.client.config.prefix}help on ${this.client.guilds.size} guilds`,
-            `My website : atlanta-bot.fr`,
-            `Add me with ${this.client.config.prefix}invite!`
+            {
+                name:`@Atlanta help on ${this.client.guilds.size} guilds`,
+                type:`LISTENING`
+            },
+            {
+                name:`my website : atlanta-bot.fr`,
+                type:`PLAYING`
+            },
+            {
+                name:`add me with ${this.client.config.prefix}invite!`,
+                type:`STREAMING`
+            }
         ];
         var client = this.client;
         var i = 0;
         setInterval(function(){
-            client.user.setActivity(games[i]);
+            client.user.setActivity(games[i].name, {type: games[i].type});
             if(games[parseInt(i + 1)]) i++;
             else i = 0;
         }, 20000);
