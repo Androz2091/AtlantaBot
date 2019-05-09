74c3bb488f6b4ab58c6a7528da9e83b74c5f60b0
Androz2091 <androz2091@gmail.com>
2019-05-09 16:17:01 +0200

    feat(website): add servers selector

--- website/dashboard/routes/discord.js
+++ website/dashboard/routes/discord.js
@@ -4,11 +4,16 @@ const passport = require("passport");
 
 // Gets login page
 router.get("/login", passport.authenticate("discord", { failureRedirect: "/login" }), function(req, res) {
-    res.redirect("/panel");
+    res.redirect("/panel/selector");
 });
 
 router.get("/", function(req, res){
     res.redirect("/login");
+});
+
+router.get("/logout", async function(req, res) {
+    await req.logout();
+    await res.redirect("/");
 });
 
 module.exports = router;
--- website/dashboard/routes/panel.js
+++ website/dashboard/routes/panel.js
@@ -1,10 +1,28 @@
 const express = require('express');  
 const router = express.Router();
 
-// Gets dashboard home page
-router.get("/", (req, res) => {
+// Gets servers selector home page
+router.get("/selector", (req, res) => {
+    // if the user is not authenticated
+    if(!req.isAuthenticated()) return res.redirect("/api/discord/login");
+    var guilds = [];
+    req.user.guilds.forEach(guild => {
+        if(!guild.owner) return
+        guilds.push({
+            url: (guild.icon ? "https://cdn.discordapp.com/icons/"+guild.id+"/"+guild.icon+".png?size=128" : "dist/img/discordcry.png"),
+            name: guild.name,
+            inviteURL: "https://discordapp.com/oauth2/authorize?client_id="+req.client.user.id+"&scope=bot&permissions=2146958847&guild_id="+guild.id
+        });
+        
+    });
     res.render("index", {
-        title:"Hello World"
+        user:{
+            avatarURL: (req.user.avatar ? "https://cdn.discordapp.com/avatars/"+req.user.id+"/"+req.user.avatar : "https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png"),
+            tag: req.user.username+"#"+req.user.discriminator,
+            ID: req.user.id,
+            client: req.client
+        },
+        guilds: guilds
     });
 });
 
