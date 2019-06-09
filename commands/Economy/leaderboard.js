15bf463edd5a428c316c75817598ba47cd6f455c
Androz2091 <androz2091@gmail.com>
2019-06-09 19:46:25 +0200

    fix(leaderboard): update levels leaderboard

--- commands/Economy/leaderboard.js
+++ commands/Economy/leaderboard.js
@@ -37,7 +37,7 @@ class Leaderboard extends Command {
                 id:user.ID,
                 credits:user.data.credits,
                 rep:user.data.rep,
-                lvl:user.data.level
+                lvl:parseInt(user.data.level)
             });
         });
 
@@ -69,7 +69,7 @@ class Leaderboard extends Command {
             order.push('rep', 'lvl', 'credits');
         }
         // Sort the array by level
-        if(args[0].toLowerCase() == 'lvl' || args[0].toLowerCase() == 'level'){
+        if(args[0].toLowerCase() == 'lvl' || args[0].toLowerCase() == 'levels'){
             leaderboard = this.client.functions.sortByKey(leaderboard, 'lvl');
             table.setHeading('#', message.language.get('USER'), message.language.get('TLEVEL'), message.language.get('TREP'), message.language.get('TCREDITS'));
             order.push('lvl', 'rep', 'credits');
@@ -103,7 +103,7 @@ class Leaderboard extends Command {
                         // Update the username of the user 
                         var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
                         var _user = user.username.replace(regex, '');
-                        if(_user.length > 20) _user = _user.substr(0, 20)
+                        if(_user.length > 20) _user = _user.substr(0, 20);
                         // Add new row to the ascii table
                         table.addRow(pos, _user, element[order[0]], element[order[1]], element[order[2]]);
                     });
