ed6e76315b2174e6b7770f9f5e6ec3f9da84a8cd
Androz2091 <androz2091@gmail.com>
2019-06-26 10:47:46 +0200

    feat(user): add economy things

--- base/User.js
+++ base/User.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 module.exports = mongoose.model("User", new mongoose.Schema({
     id: { type: String }, // Discord ID of the user
+    money: { type: Number, default: 0 }, // Money of the user
+    rep: { type: Number, default: 0 }, // Reputation points of the user
+    exp: { type: Object, default: { // Experience of the user
+        level: 0,
+        points: 0
+    }},
+    badges: { type: Array, default: [] }, // Badges of the user
     bio: { type: String }, // Biography of the user
     birthdate: { type: Date }, // Birthdate of the user (the timestamp)
     lover: { type: String }, // The person with whom the user is in a relationship
@@ -10,5 +17,6 @@ module.exports = mongoose.model("User", new mongoose.Schema({
         slots: 0,
         findwords: 0,
         number: 0
-    }}
+    }},
+    slowmode: { type: Object }
 }));
