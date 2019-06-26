de1e549b8acbd7a92d6d38c06e3f10c6b3b8b180
Androz2091 <androz2091@gmail.com>
2019-06-26 10:45:54 +0200

    fix(functions): remove old functions

--- utils/functions.js
+++ utils/functions.js
@@ -1,60 +1,6 @@
 const Discord = require('discord.js');
 
 module.exports = {
-
-    // With quick.db, it's not possible to records boolean (like false, true or null)
-
-    createGuild: function(client, guild){
-        client.databases[1].set(guild.id, {
-            id:guild.id, // The ID of the guild
-            name:guild.name, // The name of the guild (when the bot is added)
-            lang:"fr", // The language of the guild
-            prefix:client.config.prefix, // the prefix of the guild
-            commands:{}, // the guild's custom commands
-            welcome:{ status:"disabled", message:"unknow", channel:"unknow" }, // Welcome messages plugin
-            welcome_mp:{ status:"disabled", message:"unknow" }, // Welcome messages in DM plugin
-            leave:{ status:"disabled", message:"unknow", channel:"unknow" }, // Goodbye messages plugin
-            autorole:{ status:"disabled", role:"unknow" }, // Autorole plugin
-            deleteinvite:{status:"disabled",channels:[]}, // Auto delete invites links
-            ignored_channels:[], // The channels wich are ignored by the bot
-            slowmode:{}, // An object like that : { "channelid":"slowmodetime", "channelid2", "slowmodetime2"}
-            userslowmode:{}, // Used to store users data
-            warns_sanctions:{}, // Used to store users guild sanctions
-            channels:{ modlogs: 'false', suggestion: 'false' }, // Used to store channels guild settings
-            case: 0, // Used to store number of mod case
-            automod_warns: {}, // Used to store mod settings
-            muted: {} // This object is used to store muted member 
-        });
-        // Log in the console
-        client.logger.log(`New server ${guild.name} - (${guild.id}) [${guild.memberCount} members]`, 'log');
-        // return guild data object
-        return client.databases[1].get(guild.id);
-    },
-
-    createUser: function(client, user){
-        client.databases[0].set(user.id, {
-            credits:0, // The credits of the user
-            rep:0, // The reputation's point of the user
-            level:0, // The user's level
-            xp:0, // the user's xp
-            name:user.username, // The name of the user (when the bot registers it)
-            tag:user.discriminator, // The tag #0000 of the user (when the bot registers it)
-            bio:"unknow", // The user's biography (which is display on his profile)
-            birthdate:"unknow", // The birth date of the user
-            partner:"false", // The user boyfriend or girlfriend
-            old_partners:[], // The user's previous partners
-            badges:[], // the badges of the member
-            color:client.config.embed.color, // The user's embed color,
-            registeredAt:Date.now(), // The date wich the user is registered
-            // The user's statistics
-            stats:{ "commands":0, "findwords":{ "best-score":"unknow", "wins":0, }, "number":{ "best-score":"false", "wins":0, } }
-        });
-        // Log in the console
-        client.logger.log(`New user ${user.username} - (${user.id})`, 'log');
-        // Return user data object
-        return client.databases[0].get(user.id);
-    },
-
     
     /**
      * Gets the users data
@@ -63,21 +9,23 @@ module.exports = {
      * @returns The users data
      */
     async getUsersData(client, users){
-        let usersData = [];
-        users.forEach((u) => {
-            client.usersData.find({id: u.id}, function(err, result){
-                if(result[0]){
-                    usersData.push(result[0]);
-                } else {
-                    let user = new client.usersData({
-                        id: u.id
-                    });
-                    user.save();
-                    usersData.push(user);
-                }
-            });
+        return new Promise(async function(resolve, reject){
+            let usersData = [];
+            for(let u of users){
+                client.usersData.find({id: u.id}, function(err, result){
+                    if(result[0]){
+                        usersData.push(result[0]);
+                    } else {
+                        let user = new client.usersData({
+                            id: u.id
+                        });
+                        user.save();
+                        usersData.push(user);
+                    }
+                });
+            }
+            resolve(usersData);
         });
-        return usersData;
     },
 
     /**
