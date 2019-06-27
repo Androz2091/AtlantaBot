20be061e607b27d061ffc5c9abcb81d628d820c9
Androz2091 <androz2091@gmail.com>
2019-06-27 13:45:36 +0200

    fix(user): set default to null

--- base/User.js
+++ base/User.js
@@ -7,14 +7,18 @@ module.exports = mongoose.model("User", new mongoose.Schema({
     exp: { type: Number, default: 0 }, // Exp points of the user
     level: { type: Number, default: 0 }, // Level of the user
     badges: { type: Array, default: [] }, // Badges of the user
-    bio: { type: String }, // Biography of the user
-    birthdate: { type: Date }, // Birthdate of the user (the timestamp)
-    lover: { type: String }, // The person with whom the user is in a relationship
+    bio: { type: String, default: null }, // Biography of the user
+    birthdate: { type: Number, default: null }, // Birthdate of the user (the timestamp)
+    lover: { type: String, default: null }, // The person with whom the user is in a relationship
     registeredAt: { type: Number, default: Date.now() }, // Registered date of the user
     stats: { type: Object, default: { // Game stats for the user
         slots: 0,
         findwords: 0,
         number: 0
     }},
-    slowmode: { type: Object }
+    slowmode: { type: Object },
+    cooldowns: {
+        work: 0,
+        rep: 0
+    }
 }));
