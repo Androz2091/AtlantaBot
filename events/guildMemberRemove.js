612b70995fbce11923fb99b1cd397f524a735bbd
Androz2091 <androz2091@gmail.com>
2019-06-27 14:05:51 +0200

    fix(guildMemberRemove): mongodb update

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,39 +1,42 @@
-// The MESSAGE event runs anytime a message is received
-// Note that due to the binding of client to every event, every event
-// goes `client, other, args` when this function is run.
-
 module.exports = class {
+
     constructor (client) {
       this.client = client;
     }
   
     async run (member) {
-        // Gets the guild data
-        var guild_data = this.client.databases[1].get(member.guild.id) || this.client.functions.createGuild(member.guild);
 
-        // Check if leave message is enabled
-        if(guild_data.leave.status == 'on'){
-            var channel = member.guild.channels.get(guild_data.leave.channel);
-            if(channel){
-                // Gets and edits the message
-                var message = guild_data.leave.message
-                    .replace(/{user}/g, `${member.user.username}#${member.user.discriminator}`)
-                    .replace(/{server}/g, member.guild.name)
-                    .replace(/{membercount}/g, member.guild.memberCount)
-                
-                if(guild_data.leave.withImage === 'true'){
-                    var lang = new(require('../languages/'+guild_data.lang+'.js'));
-                    var text = lang.get('LEAVE_IMG', member.guild.name);
-                    const { body } = await require('snekfetch').get(encodeURI(`https://dev.anidiots.guide/greetings/unified?type=goodbye&version=gearz&message=${text}&bot=${member.user.bot}&avatar=${member.user.displayAvatarURL}&username=${member.user.username}&discriminator=${member.user.discriminator}&guildName=${member.guild.name}&memberCount=${member.guild.memberCount}`)).set("Authorization", this.client.config.apiKeys.anidiots);
-                    channel.send(message, {
-                        files: [{
-                            attachment: Buffer.from(body.data),
-                            name: "gdb.png"
-                        }]
-                    });
-                } else channel.send(message);
+        member.guild.fetch().then(async (guild) => {
+
+            let settings = await this.client.functions.getSettings(this.client, guild.channels.first());
+
+            // Check if goodbye message is enabled
+            if(settings.plugins.goodbye.enabled){
+                let channel = guild.channels.get(settings.plugins.leave.channel);
+                if(channel){
+                    let message = settings.plugins.leave.message
+                    .replace(/{user}/g, member.user.tag)
+                    .replace(/{server}/g, guild.name)
+                    .replace(/{membercount}/g, guild.memberCount);
+                    
+                    if(settings.plugins.leave.withImage){
+                        let lang = new(require(`../languages/${settings.language}.js`));
+                        let text = lang.get("LEAVE_IMG", member.guild.name);
+                        let URL = encodeURI(`https://dev.anidiots.guide/greetings/unified?type=goodbye&version=gearz&message=${text}&bot=${member.user.bot}&avatar=${member.user.displayAvatarURL}&username=${member.user.username}&discriminator=${member.user.discriminator}&guildName=${guild.name}&memberCount=${guild.memberCount}`);
+                        let res = await fetch(URL, { headers: { "Authorization": guild.client.config.apiKeys.anidiots } });
+                        channel.send(message, {
+                            files: [{
+                                attachment: res.body,
+                                name: "goodbye.png"
+                            }]
+                        });
+                    } else {
+                        channel.send(message);
+                    }
+                }
             }
-        }
+
+        });
     }
   };
   
