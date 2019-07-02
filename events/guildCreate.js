2013f88ec2b8ed7baa2f27723c9617248e04bbbc
Androz <androz2091@gmail.com>
2019-07-02 10:15:59 +0200

    fix(guildCreate): update embeds

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -10,22 +10,22 @@ module.exports = class {
         
         guild = await guild.fetch();
 
-        let embed = new Discord.MessageEmbed()
+        let thanksEmbed = new Discord.MessageEmbed()
             .setAuthor("Thank you for adding me to your guild !")
             .setDescription("To configure me, type `"+this.client.config.prefix+"help` and look at the administration commands!\nTo change the language, type `"+this.client.config.prefix+"setlang [language]`.")
             .setColor(this.client.config.embed.color)
             .setFooter(this.client.config.embed.footer)
             .setTimestamp();
-        guild.owner.send(embed).catch((err) => {});
+        guild.owner.send(thanksEmbed).catch((err) => {});
 
         let text = "J'ai rejoint **"+guild.name+"**, avec **"+guild.members.filter((m) => !m.user.bot).size+"** membres (et "+guild.members.filter((m) => m.user.bot).size+" bots)";
 
         // Sends log embed in the logs channel
-        let embed = new Discord.MessageEmbed()
+        let logsEmbed = new Discord.MessageEmbed()
             .setAuthor(guild.name, guild.iconURL())
             .setColor("#32CD32")
             .setDescription(text);
-        this.client.channels.get(this.client.config.support.logs).send(embed);
+        this.client.channels.get(this.client.config.support.logs).send(logsEmbed);
         
     }
 }  
