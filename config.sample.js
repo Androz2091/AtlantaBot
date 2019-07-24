d566e73afb2476c4dfa17763d649d791dc30e0ab
Androz2091 <androz2091@gmail.com>
2019-07-24 15:01:35 +0200

    feat(config): add dashboard settings

--- config.sample.js
+++ config.sample.js
@@ -6,6 +6,16 @@ module.exports = {
 		id: "XXXXXXXXXXX", // The ID of the support server
 		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
 	},
+	/* Dashboard configuration */
+	dashboard: {
+		enabled: false, // whether the dashboard is enabled or not
+		secret: "XXXXXXXXXXX", // Your discord client secret
+		dashboardURL: "https://dashboard.atlanta-bot.fr", // The base URl of the dashboard
+		logs: "XXXXXXXXXXX", // The channel ID of logs
+		port: 8080, // Dashboard port
+		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
+		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
+	},
 	mongoDB: "mongodb://localhost:27017/AtlantaBot", // The URl of the mongodb database
 	prefix: "*", // The default prefix for the bot
 	/* For the embeds (embeded messages) */
