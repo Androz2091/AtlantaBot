67b0b46060b01a774c6ac491cd4eab02a33e14ec
Androz2091 <androz2091@gmail.com>
2019-07-25 16:33:15 +0200

    feat(dashboard): add fetchUser function

--- dashboard/routes/home.js
+++ dashboard/routes/home.js
@@ -2,8 +2,20 @@ const express = require("express"),
 CheckAuth = require("../auth/CheckAuth"),
 router = express.Router();
 
-router.get("/:lang/", CheckAuth, (req, res) => {
-    res.send(req.language);
+router.get("/", CheckAuth, async (req, res) => {
+    let userInfos = await fetchUser(req.user, req.client);
+    res.render("dashboard", {
+        user: userInfos,
+        language: req.language
+    });
 });
 
 module.exports = router;
+
+async function fetchUser(userData, client){
+    let user = await client.users.fetch(userData.id);
+    let usersDb = await client.functions.getUsersData(client, [ user ]);
+    let userDb = usersDb[0].toJSON()
+    let userInfos = { ...user.toJSON(), ...userDb, ...userData, ...user.presence };
+    return userInfos;
+}
