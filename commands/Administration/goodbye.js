678a454ebe646ddf1c113a20843f8b5a43c04a0a
xixi52 <one.com.suspendu@gmail.com>
2019-07-27 11:29:49 +0000

    Add custom colors for goodbye images

--- commands/Administration/goodbye.js
+++ commands/Administration/goodbye.js
@@ -27,13 +27,46 @@ class Goodbye extends Command {
             message.client.emit("guildMemberRemove", message.member);
             return message.channel.send(message.language.get("GOODBYE_TEST_SUCCESS"));
         }
+        if(args[0] === "color" && data.settings.plugins.goodbye.enabled && data.settings.plugins.goodbye.image.enabled){
+            //Color ?
+            if(args[1]){
+                // Hexadecimal value
+                if(args[1].startsWith("#")){
+                    let color = args[1],
+                    hexacolor = color.replace(/[^a-z0-9]/gi, "");
+                    if(hexacolor.length === 6){
+                        data.settings.plugins.goodbye.image.color = "#"+hexacolor.toUpperCase();
+                        data.settings.markModified("plugins.goodbye");
+                        data.settings.save();
+                        return message.channel.send(message.language.get("GOODBYE_SUCCESS_COLOR", data.settings.plugins.goodbye.image.color, data.settings.prefix));
+                    } else {
+                        return message.channel.send(message.language.get("GOODBYE_ERR_COLOR"));
+                    }
+                }
+                // Defaults values
+                if(args[1] === "white" || args[1] === "orange" || args[1] === "red" || args[1] === "blue" || args[1] === "green" || args[1] === "purple"){
+                    data.settings.plugins.goodbye.image.color = args[1].toLowerCase();
+                    data.settings.markModified("plugins.goodbye");
+                    data.settings.save();
+                    return message.channel.send(message.language.get("GOODBYE_SUCCESS_COLOR", data.settings.plugins.goodbye.image.color, data.settings.prefix));
+                } else {
+                    return message.channel.send(message.language.get("GOODBYE_ERR_COLOR"));
+
+                }
+            } else {
+                return message.channel.send(message.language.get("GOODBYE_ERR_COLOR"));
+            } 
+        }
 
         if(data.settings.plugins.goodbye.enabled){
             data.settings.plugins.goodbye = {
                 enabled: false,
                 message: null,
                 channel: null,
-                withImage: null
+                image: {
+                    enabled: false,
+                    color: null
+                }
             };
             data.settings.markModified("plugins.goodbye");
             data.settings.save();
@@ -44,7 +77,10 @@ class Goodbye extends Command {
             enabled: true,
             channel: null,
             message: null,
-            withImage: null,
+            image: {
+                enabled: false,
+                color: null
+            }
         };
 
         message.channel.send(message.language.get("GOODBYE_FORM_CHANNEL", message.author.username));
@@ -52,17 +88,13 @@ class Goodbye extends Command {
 
         collector.on("collect", async (msg) => {
 
-            if(goodbye.message){
+            if(goodbye.message && !goodbye.image.enabled){
                 if(msg.content.toLowerCase() === message.language.get("UTILS").YES.toLowerCase()){
-                    goodbye.withImage = true;
-                    message.channel.send(message.language.get("GOODBYE_FORM_SUCCESS", goodbye.channel, data.settings.prefix));
-                    data.settings.plugins.goodbye = goodbye;
-                    data.settings.markModified("plugins.goodbye");
-                    await data.settings.save();
-                    return collector.stop();
+                    goodbye.image.enabled = true;
+                    return message.channel.send(message.language.get("GOODBYE_FORM_COLOR"));
                 }
                 if(msg.content.toLowerCase() === message.language.get("UTILS").NO.toLowerCase()){
-                    goodbye.withImage = false;
+                    goodbye.image.enabled = false;
                     message.channel.send(message.language.get("GOODBYE_FORM_SUCCESS", goodbye.channel, data.settings.prefix));
                     data.settings.plugins.goodbye = goodbye;
                     data.settings.markModified("plugins.goodbye");
@@ -70,6 +102,33 @@ class Goodbye extends Command {
                     return collector.stop();
                 }
                 return message.channel.send(message.language.get("ERR_YES_NO"));
+            }
+
+            if(goodbye.image.enabled){
+                if(msg.content.startsWith("#")){
+                    let color = msg.content,
+                    hexacolor = color.replace(/[^a-z0-9]/gi, "");
+                    if(hexacolor.length === 6){
+                        goodbye.image.color = "#"+hexacolor.toUpperCase();
+                        message.channel.send(message.language.get("GOODBYE_FORM_SUCCESS_COLOR", goodbye.image.color, goodbye.channel, data.settings.prefix));
+                        data.settings.plugins.goodbye = goodbye;
+                        data.settings.markModified("plugins.goodbye");
+                        await data.settings.save();
+                        return collector.stop();
+                    } else {
+                        return message.channel.send(message.language.get("GOODBYE_ERR_COLOR"));
+                    }
+                }
+                if(msg.content === "white" || msg.content === "orange" || msg.content === "red" || msg.content === "blue" || msg.content === "green" || msg.content === "purple"){
+                    goodbye.image.color = msg.content.toLowerCase();
+                    message.channel.send(message.language.get("GOODBYE_FORM_SUCCESS_COLOR", goodbye.image.color, goodbye.channel, data.settings.prefix));
+                    data.settings.plugins.goodbye = goodbye;
+                    data.settings.markModified("plugins.goodbye");
+                    await data.settings.save();
+                    return collector.stop();
+                } else {
+                    return message.channel.send(message.language.get("GOODBYE_ERR_COLOR"));
+                }
             }
 
             if(goodbye.channel && !goodbye.message){
