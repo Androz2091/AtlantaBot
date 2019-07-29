2a21746e04cdfe2343ee2f79a6a16cbb55f2ad9d
Androz2091 <androz2091@gmail.com>
2019-07-29 10:54:07 +0200

    Save guild in logs

--- base/Log.js
+++ base/Log.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model("Log", new mongoose.Schema({
     command: { type: Object },
     date: { type: Number, default: Date.now() },
-    user: { type: Object }
+    user: { type: Object },
+    guild: { type: Object }
 }));
