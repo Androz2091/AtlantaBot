5c3463ed6c55400899880cc80666d03866760aba
Androz2091 <androz2091@gmail.com>
2019-08-28 20:27:53 +0200

    fix(log): delete log model

--- base/Log.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Log", new mongoose.Schema({
-    command: { type: Object },
-    date: { type: Number, default: Date.now() },
-    user: { type: Object },
-    guild: { type: Object }
-}));
