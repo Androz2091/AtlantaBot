9279a03e9df74714e53f303ef6ead517b0cee902
Androz2091 <androz2091@gmail.com>
2019-08-29 13:27:43 +0200

    feat(withdraw): add new withdraw command

---
+++ commands/Economy/withdraw.js
@@ -0,0 +1,45 @@
+const Command = require("../../base/Command.js"),
+Discord = require("discord.js");
+
+class Withdraw extends Command {
+
+    constructor (client) {
+        super(client, {
+            name: "withdraw",
+            description: (language) => language.get("WITHDRAW_DESCRIPTION"),
+            usage: (language) => language.get("WITHDRAW_USAGE"),
+            examples: (language) => language.get("WITHDRAW_EXAMPLES"),
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: [],
+            memberPermissions: [],
+            botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 1000
+        });
+    }
+
+    async run (message, args, data) {
+        
+        let amount = args[0];
+        if(isNaN(amount) || parseInt(amount, 10) < 1){
+            return message.channel.send(message.language.get("WITHDRAW_ERR_AMOUNT"));
+        }
+        amount = parseInt(amount, 10);
+        
+        if(data.memberData.bankSold < amount){
+            return message.channel.send(message.language.get("WITHDRAW_ERR_AMOUNT_TOO_HIGH", amount));
+        }
+
+        data.memberData.money = data.memberData.money + amount;
+        data.memberData.bankSold = data.memberData.bankSold - amount;
+        data.memberData.save();
+
+        message.channel.send(message.language.get("WITHDRAW_SUCCESS", amount));
+    }
+
+}
+
+module.exports = Withdraw;
