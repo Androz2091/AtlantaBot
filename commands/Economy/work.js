2345bf3c3349ae295fa08275adace428c885f57a
Androz2091 <androz2091@gmail.com>
2019-08-29 17:36:57 +0200

    feat(work): add work streak

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -34,19 +34,48 @@ class Work extends Command {
             }
         }
 
+        if(Date.now() > data.memberData.cooldowns.work+(24*3600000)){
+            data.memberData.workStreak = 0;
+        }
+
         // Records in the database the time when the member will be able to execute the command again (in 12 hours)
         let toWait = Date.now() + 21600000;
         data.memberData.cooldowns.work = toWait;
+        data.memberData.markModified("cooldowns");
+
+        data.memberData.workStreak = (data.memberData.workStreak || 0) + 1;
+        await data.memberData.save();
 
         let embed = new Discord.MessageEmbed()
-            .setAuthor(message.language.get("WORK_CLAIMED_TITLE"), message.author.displayAvatarURL())
-            .setDescription( message.language.get("WORK_CLAIMED_CONTENT"))
-            .setFooter(data.config.embed.footer)
-            .setColor(data.config.embed.color)
-            .setTimestamp();
+            .setFooter(message.language.get("WORK_FOOTER"), message.author.displayAvatarURL())
+            .setColor(data.config.embed.color);
+        
+        let award = [
+            this.client.config.emojis.letters.a,
+            this.client.config.emojis.letters.w,
+            this.client.config.emojis.letters.a,
+            this.client.config.emojis.letters.r,
+            this.client.config.emojis.letters.d
+        ];
+        let won = 200;
 
-        data.memberData.markModified("cooldowns");
-        data.memberData.money += 200;
+        if((data.memberData.workStreak || 0) >= 5){
+            won+=200;
+            embed.addField(message.language.get("WORK_CLAIMED_HEADINGS")[0], message.language.get("WORK_CLAIMED_SALARY", won))
+            .addField(message.language.get("WORK_CLAIMED_HEADINGS")[1], message.language.get("WORK_AWARD"));
+            data.memberData.workStreak = 0;
+        } else {
+            for(let i = 0; i < award.length; i++){
+                if(data.memberData.workStreak > i){
+                    let letter = Discord.Util.parseEmoji(award[i]).name.split("_")[1];
+                    award[i] = ":regional_indicator_"+letter+":";
+                }
+            }
+            embed.addField(message.language.get("WORK_CLAIMED_HEADINGS")[0], message.language.get("WORK_CLAIMED_SALARY", won))
+            .addField(message.language.get("WORK_CLAIMED_HEADINGS")[1], award.join(""));
+        }
+
+        data.memberData.money = data.memberData.money + 200;
         data.memberData.save();
 
         // Send the embed in the current channel
