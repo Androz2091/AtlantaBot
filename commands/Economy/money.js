0c646d2631da576edceefee5bd2cc9db74af856a
xixi52 <51019403+xixi52@users.noreply.github.com>
2019-09-04 22:26:10 +0200

    Update(money) Add bankSold, globalMoney and money

--- commands/Economy/money.js
+++ commands/Economy/money.js
@@ -1,6 +1,12 @@
 const Command = require("../../base/Command.js"),
 Discord = require("discord.js");
 
+const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
 class Credits extends Command {
 
     constructor (client) {
@@ -23,19 +29,30 @@ class Credits extends Command {
 
     async run (message, args, data) {
         
-        let user = message.mentions.users.size > 0 ? message.mentions.users.first() : message.author;
+        let user = message.mentions.users.size > 0 ? message.mentions.users.first() : message.author,
+        client = this.client;
 
         if(user.bot){
             return message.channel.send(message.language.get("ERR_BOT_USER"));
         }
 
-        let userData = (message.author === user) ? data.memberData : await this.client.findOrCreateMember({ id: user.id, guildID: message.guild.id }); 
+        let memberData = (message.author === user) ? data.memberData : await client.findOrCreateMember({ id: user.id, guildID: message.guild.id }); 
+
+        let commonsGuilds = client.guilds.filter((g) => g.members.get(user.id));
+        let globalMoney = 0;
+        await asyncForEach(commonsGuilds.array(), async (guild) => {
+            let memberData = await client.findOrCreateMember({ id: user.id, guildID: guild.id });
+            globalMoney+=memberData.money;
+            globalMoney+=memberData.bankSold;
+        });
 
         let embed = new Discord.MessageEmbed()
             .setAuthor(message.language.get("CREDITS_TITLE", user.tag), user.displayAvatarURL())
+            .addField(message.language.get("PROFILE_HEADINGS").MONEY, message.language.get("DISPLAY_MONEY", Math.ceil(memberData.money)), true)
+            .addField(message.language.get("PROFILE_HEADINGS").BANK, message.language.get("DISPLAY_MONEY",  Math.ceil(memberData.bankSold)), true)
+            .addField(message.language.get("PROFILE_HEADINGS").GLOBAL_MONEY, message.language.get("DISPLAY_MONEY",  Math.ceil(globalMoney)), true)
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer)
-            .setDescription(message.language.get("CREDITS_CONTENT", userData.money, user.username))
         message.channel.send(embed);
     }
 
