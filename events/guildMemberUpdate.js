a590bc109214914f8fdd30102938bb4c7fa1443c
Androz2091 <androz2091@gmail.com>
2019-10-06 08:41:08 +0200

    feat(achievements): add tip achievement

---
+++ events/guildMemberUpdate.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = class {
+
+    constructor (client) {
+      this.client = client;
+    }
+  
+    async run (oldMember, newMember) {
+        if(oldMember.guild.id !== this.client.config.support.id) return;
+        if(oldMember.roles.some((r) => r.name === "【💳】Donators")) return;
+        if(newMember.roles.some((r) => r.name === "【💳】Donators")){
+            let userData = await this.client.findOrCreateUser({ id: newMember.id });
+            userData.achievements.tip.progress.now = 1;
+            userData.achievements.tip.achieved = true;
+            userData.markModified("achievements.tip");
+            await userData.save();
+            newMember.send({ files: [ { name: "unlocked.png", attachment: "./assets/img/achievements/achievement_unlocked5.png"} ] });
+        }
+    }
+};
