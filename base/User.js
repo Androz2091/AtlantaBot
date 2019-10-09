445a03dec6bdb99fcc439b3532e269874ce93411
Androz2091 <androz2091@gmail.com>
2019-10-09 18:53:59 +0200

    feat(user): add apitoken property

--- base/User.js
+++ base/User.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose"),
 Canvas = require("canvas");
 
+const genToken = () => {
+    let token = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwzy0123456789.-_";
+    for (let i = 0; i < 32; i++){
+        token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
+};
+
 const userSchema = new mongoose.Schema({
 
     /* REQUIRED */
@@ -76,8 +85,15 @@ const userSchema = new mongoose.Schema({
     /* OTHER INFORMATIONS */
     afk: { type: String, default: null }, // Whether the member is AFK
     reminds: { type: Array, default: [] }, // the reminds of the user
-    logged: { type: Boolean, default: false } // if the user is logged to the dashboard
+    logged: { type: Boolean, default: false }, // if the user is logged to the dashboard
+    apiToken: { type: String, default: genToken() } // the api token of the user
 
+});
+
+userSchema.method("genApiToken", async function(){
+    this.apiToken = genToken();
+    await this.save();
+    return this.apiToken;
 });
 
 userSchema.method("getAchievements", async function(){
