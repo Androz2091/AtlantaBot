56049c864a8902ee8e3bd4aa47a78d2acd3dbb29
Androz2091 <androz2091@gmail.com>
2019-12-29 10:35:51 +0100

    fix: use google translation instead of deepl

--- commands/General/translate.js
+++ commands/General/translate.js
@@ -1,6 +1,8 @@
 const Command = require("../../base/Command.js"),
 Discord = require("discord.js"),
-DeepL = require("deepl-scraper"); // Thanks to @Kaki87 :)
+translate = require("@k3rn31p4nic/google-translate-api");
+
+const langs = ["afrikaans", "albanian", "amharic", "arabic", "armenian", "azerbaijani", "bangla", "basque", "belarusian", "bengali", "bosnian", "bulgarian", "burmese", "catalan", "cebuano", "chichewa", "corsican", "croatian", "czech", "danish", "dutch", "english", "esperanto", "estonian", "filipino", "finnish", "french", "frisian", "galician", "georgian", "german", "greek", "gujarati", "haitian creole", "hausa", "hawaiian", "hebrew", "hindi", "hmong", "hungarian", "icelandic", "igbo", "indonesian", "irish", "italian", "japanese", "javanese", "kannada", "kazakh", "khmer", "korean", "kurdish (kurmanji)", "kyrgyz", "lao", "latin", "latvian", "lithuanian", "luxembourgish", "macedonian", "malagasy", "malay", "malayalam", "maltese", "maori", "marathi", "mongolian", "myanmar (burmese)", "nepali", "norwegian", "nyanja", "pashto", "persian", "polish", "portugese", "punjabi", "romanian", "russian", "samoan", "scottish gaelic", "serbian", "sesotho", "shona", "sindhi", "sinhala", "slovak", "slovenian", "somali", "spanish", "sundanese", "swahili", "swedish", "tajik", "tamil", "telugu", "thai", "turkish", "ukrainian", "urdu", "uzbek", "vietnamese", "welsh", "xhosa", "yiddish", "yoruba", "zulu"];
 
 class Translate extends Command {
 
@@ -24,44 +26,19 @@ class Translate extends Command {
 
     async run (message, args, data) {
         
-        let langs = [
-            { name: "en", aliases: [ "english" ] },
-            { name: "de", aliases: [ "german" ] },
-            { name: "fr", aliases: [ "french" ] },
-            { name: "es", aliases: [ "spanish" ] },
-            { name: "pt", aliases: [ "portuguese" ] },
-            { name: "it", aliases: [ "italian" ] },
-            { name: "nl", aliases: [ "dutch" ] },
-            { name: "pl", aliases: [ "polish" ] },
-            { name: "ru", aliases: [ "russian" ] }
-        ];
+        if(args[0] === "langs-list"){
+            let index = 0;
+            let langsList = "```Css\n"+(langs.map((l, i) => `#${i+1} - ${l}`).join("\n"))+"```";
+            message.author.send(langsList);
+            return message.channel.send(message.language.get("TRANSLATE_LANGS"));
+        }
         
         let pWait = await message.channel.send(message.language.get("UTILS").PLEASE_WAIT);
         
         if(!args[0]){
             return pWait.edit(message.language.get("TRANSLATE_ERR_LANG", data.guild.prefix));
         }
-
-        // If the user wants the languages list
-        if(args[0] === "langs-list"){
-            let index = 1;
-            let messages = [];
-            let current = "```Css\n";
-            langs.forEach((lang) => {
-                if(current.length > 1700){
-                    current += "\n```";
-                    messages.push(current);
-                    current = "";
-                }
-                current += `${index++} ${lang.name} (${lang.aliases.map((lang) => lang).join(", ")})\n`;
-            });
-            messages.push(current += "\n```");
-            messages.forEach((m) => {
-                message.author.send(m);
-            });
-            return pWait.edit(message.language.get("TRANSLATE_LANGS"));
-        }
-        
+    
         if(!args[1]){
             return pWait.edit(message.language.get("TRANSLATE_ERR_MSG"));
         }
@@ -70,21 +47,16 @@ class Translate extends Command {
         let language = args[0].toLowerCase();
         let toTranslate = args.slice(1).join(" ");
         
-        if(!langs.find((lang) => lang.name === language)){
+        if(!langs.includes(language)){
             return pWait.edit(message.language.get("TRANSLATE_ERR_NOT_FOUND", data.guild.prefix, language));
         }
-
-        let isAlias = langs.find((lang) => lang.aliases.includes(language));
-        if(isAlias){
-            language = isAlias.name;
-        }
         
-        let translated = await DeepL.translate(toTranslate, null, language);
+        let translated = await translate(toTranslate, { to: language });
 
         let resEmbed = new Discord.MessageEmbed()
             .setAuthor("Translator", message.client.user.displayAvatarURL)
-            .addField(langs.find((lang) => lang.name === translated.source.lang).aliases[0], "```"+toTranslate+"```")
-            .addField(langs.find((lang) => lang.name === translated.target.lang).aliases[0], "```"+translated.target.translation+"```")
+            .addField(translated.from.language.iso, "```"+toTranslate+"```")
+            .addField(language, "```"+translated.text+"```")
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer);
 
