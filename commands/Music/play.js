b92fd6ef16beb4c6724eb9259393b65a70df369f
Androz2091 <androz2091@gmail.com>
2020-01-18 19:13:10 +0100

    player: add play command

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -27,6 +27,8 @@ class Play extends Command {
             return message.channel.send(message.language.get("ERR_COMMAND_DISABLED"));
         }
 
+        let isPlaying = this.client.player.isPlaying(message.guild.id);
+
         let name = args.join(" ");
         if(!name){
             return message.channel.send(message.language.get("PLAY_ERR_NO_NAME"));
@@ -40,39 +42,56 @@ class Play extends Command {
         }
 
         // Check my permissions
-        var perms = voice.permissionsFor(message.client.user);
+        let perms = voice.permissionsFor(message.client.user);
         if(!perms.has("CONNECT") || !perms.has("SPEAK")){
             return message.channel.send(message.language.get("PLAY_ERR_PERMS"));
         }
 
         let youtube = this.client.player.SYA;
-        let video = await youtube.getVideo(url).catch((err) => {});
+        let video = null;
+
+        video = await youtube.getVideo(url).catch((err) => {});
+
+        if(!video){
+            try {
+                let videos = await youtube.searchVideos(name, 7);
+                let i = 0;
+                let embed = new Discord.MessageEmbed()
+                    .addField(message.language.get("PLAY_HEADINGS")[3], videos.map((v) => `**${++i} -** ${v.title}`).join("\n")+"\n\n\n"+message.language.get("PLAY_SEARCH"))
+                    .setColor(data.config.embed.color)
+                    .setFooter(data.config.embed.footer);
+                message.channel.send(embed);
+                await message.channel.awaitMessages((m) => m.content > 0 && m.content < 8, { max: 1, time: 20000, errors: ["time"] }).then(async (answers) => {
+                    let index = parseInt(answers.first().content, 10);
+                    video = await youtube.getVideoByID(videos[index-1].id);
+                    video = url;
+                }).catch((e) => {
+                    console.log(e);
+                    return message.channel.send(message.language.get("PLAY_ERR_TIMEOUT"));
+                });
+            } catch(e){
+                console.log(e);
+                return message.channel.send(message.language.get("PLAY_ERR_NOT_FOUND"));
+            }
+        }
 
         if(video){
-            return this.client.player.play(voice, url);
-        }
-
-        try {
-            let videos = await youtube.searchVideos(name, 7);
-            let i = 0;
-            let embed = new Discord.MessageEmbed()
-                .addField(message.language.get("PLAY_HEADINGS")[3], videos.map((v) => `**${++i} -** ${v.title}`).join("\n")+"\n\n\n"+message.language.get("PLAY_SEARCH"))
-                .setColor(data.config.embed.color)
-                .setFooter(data.config.embed.footer);
-            message.channel.send(embed);
-            await message.channel.awaitMessages((m) => m.content > 0 && m.content < 8, { max: 1, time: 20000, errors: ["time"] }).then(async (answers) => {
-                let index = parseInt(answers.first().content, 10);
-                video = await youtube.getVideoByID(videos[index-1].id);
-                return this.client.player.play(voice, video.url);
-            }).catch((e) => {
-                console.log(e);
-                return message.channel.send(message.language.get("PLAY_ERR_TIMEOUT"));
-            });
-        } catch(e){
-            console.log(e);
+            if(isPlaying){
+                let song = await this.client.player.addToQueue(message.guild.id, video);
+                message.channel.send(message.language.get("PLAY_ADDED_TO_QUEUE", song));
+            } else {
+                let song = await this.client.player.play(voice, video);
+                song.queue.on("end", () => {
+                    message.channel.send(message.language.get("PLAY_ERR_NO_SONG"));
+                })
+                .on("songChanged", (oldSong, newSong) => {
+                    message.channel.send(message.language.get("PLAY_SUCCESS", newSong));
+                });
+                message.channel.send(message.language.get("PLAY_SUCCESS", song));
+            }
+        } else {
             return message.channel.send(message.language.get("PLAY_ERR_NOT_FOUND"));
         }
-
     }
 
 }
