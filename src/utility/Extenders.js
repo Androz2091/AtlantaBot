7998feb1a2ad61bab3ef9505ac3f5e5a3e757ebb
Androz2091 <androz2091@gmail.com>
2020-02-13 22:20:10 +0100

    style(extenders): prettier fixes

--- src/utility/Extenders.js
+++ src/utility/Extenders.js
@@ -1,11 +1,11 @@
 const { Message } = require("discord.js");
 const Constants = require("../utility/Constants");
 
-Message.prototype.translate = function(key, args){
+Message.prototype.translate = function(key, args) {
     const language = this.client.translate.get(
-        this.guild ? this.guild.settings.language : 'en-US'
+        this.guild ? this.guild.settings.language : "en-US"
     );
-    if (!language) throw 'Message: Invalid language set in settings.';
+    if (!language) throw "Message: Invalid language set in settings.";
     return language(key, args);
 };
 
@@ -15,22 +15,24 @@ Message.prototype.error = function(content, send) {
 };
 
 Message.prototype.success = function(content, send) {
-    const updatedContent = `${Constants.Emojis.SUCCESS} | ${content}`
+    const updatedContent = `${Constants.Emojis.SUCCESS} | ${content}`;
     return send ? this.channel.send(updatedContent) : updatedContent;
 };
 
 // Translate and send the message
 Message.prototype.sendT = function(key, args, type) {
-    if(type === 'error') return this.error(this.translate(key, args));
-    if(type === 'success') return this.success(this.translate(key, args));
+    if (type === "error") return this.error(this.translate(key, args));
+    if (type === "success") return this.success(this.translate(key, args));
     else return this.channel.send(this.translate(key, args));
 };
 
 // Translate and edit the message
 Message.prototype.editT = function(key, args, type) {
-    let updatedContent = '';
-    if(type === 'error') updatedContent = this.error(this.translate(key, args), false);
-    if(type === 'success') updatedContent = this.success(this.translate(key, args), false);
+    let updatedContent = "";
+    if (type === "error")
+        updatedContent = this.error(this.translate(key, args), false);
+    if (type === "success")
+        updatedContent = this.success(this.translate(key, args), false);
     else updatedContent = this.translate(key, args);
     return this.edit(updatedContent);
-}
+};
