782b9d79eae8651a32d8c433fc0d9bee46d84507
Androz2091 <androz2091@gmail.com>
2020-02-13 15:19:20 +0100

    Add message.error() and message.success()

--- src/utility/Extenders.js
+++ src/utility/Extenders.js
@@ -1,5 +1,18 @@
 const { Message } = require("discord.js");
+const Constants = require("../utility/Constants");
+
+Message.prototype.translate = function(key, args){
+    const language = this.client.translate.get(
+        this.guild ? this.guild.settings.language : 'en-US'
+    );
+    if (!language) throw 'Message: Invalid language set in settings.';
+    return language(key, args);
+};
 
 Message.prototype.error = function(content) {
-    this.channel.send();
+    this.channel.send(`${Constants.Emojis.ERROR} | ${content}`);
+};
+
+Message.prototype.success = function(content) {
+    this.channel.send(`${Constants.Emojis.SUCCESS} | ${content}`);
 };
