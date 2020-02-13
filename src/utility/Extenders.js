39c02fe6d28eea94c8a3cd65ce2f05d600978507
Androz2091 <androz2091@gmail.com>
2020-02-13 18:11:38 +0100

    extenders: add sendT and editT function

--- src/utility/Extenders.js
+++ src/utility/Extenders.js
@@ -9,10 +9,28 @@ Message.prototype.translate = function(key, args){
     return language(key, args);
 };
 
-Message.prototype.error = function(content) {
-    this.channel.send(`${Constants.Emojis.ERROR} | ${content}`);
+Message.prototype.error = function(content, send) {
+    const updatedContent = `${Constants.Emojis.ERROR} | ${content}`;
+    return send ? this.channel.send(updatedContent) : updatedContent;
 };
 
-Message.prototype.success = function(content) {
-    this.channel.send(`${Constants.Emojis.SUCCESS} | ${content}`);
+Message.prototype.success = function(content, send) {
+    const updatedContent = `${Constants.Emojis.SUCCESS} | ${content}`
+    return send ? this.channel.send(updatedContent) : updatedContent;
 };
+
+// Translate and send the message
+Message.prototype.sendT = function(key, args, type) {
+    if(type === 'error') return this.error(this.translate(key, args));
+    if(type === 'success') return this.success(this.translate(key, args));
+    else return this.channel.send(this.translate(key, args));
+};
+
+// Translate and edit the message
+Message.prototype.editT = function(key, args, type) {
+    let updatedContent = '';
+    if(type === 'error') updatedContent = this.error(this.translate(key, args), false);
+    if(type === 'success') updatedContent = this.success(this.translate(key, args), false);
+    else updatedContent = this.translate(key, args);
+    return this.edit(updatedContent);
+}
