615d0c8b98ede722153817e16923c566c4de713b
Androz2091 <androz2091@gmail.com>
2020-02-16 10:20:08 +0100

    dashboard: update main route

--- src/dashboard/routes/index.js
+++ src/dashboard/routes/index.js
@@ -1,17 +1,13 @@
-const express = require("express"),
-CheckAuth = require("../auth/CheckAuth"),
-router = express.Router();
+const { Router } = require('express');
+const CheckAuth = require("../middlewares/CheckAuth");
 
-router.get("/", CheckAuth, async (req, res) => {
-    res.redirect("/selector");
-});
+module.exports.Router = class Index extends Router {
+    constructor() {
+        super();
+        this.get('/', CheckAuth, (_req, res) => {
+            res.redirect('/selector');
+        });
+    }
+};
 
-router.get("/selector", CheckAuth, async(req, res) => {
-    res.render("selector", {
-        user: req.userInfos,
-        language: req.language,
-        currentURL: `${req.client.config.dashboard.baseURL}/${req.originalUrl}`
-    });
-});
-
-module.exports = router;
+module.exports.name = '/';
