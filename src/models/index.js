b639067cb44a12026c29b75665744711d08b8465
Androz2091 <androz2091@gmail.com>
2020-02-16 10:16:45 +0100

    db: export user model

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,11 @@
 module.exports = {
+
+    // User
+    User: require("./User"),
+
+    // Guild
     Guild: require("./Guild"),
+    // Guild plugins
     AutomodPlugin: require("./AutomodPlugin"),
     WelcomePlugin: require("./WelcomePlugin")
 };
