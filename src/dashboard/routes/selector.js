385d2f7e3de14895828cf5c919e130bb6fb28a77
Androz2091 <androz2091@gmail.com>
2020-02-16 14:06:19 +0100

    dashboard: add selector route

---
+++ src/dashboard/routes/selector.js
@@ -0,0 +1,22 @@
+const { Router } = require('express');
+const CheckAuth = require("../middlewares/CheckAuth");
+const FetchUser = require("../middlewares/FetchUser");
+const Constants = require("../../utility/Constants");
+
+module.exports.Router = class Selector extends Router {
+    constructor() {
+        super();
+        this.get('/', [CheckAuth, FetchUser], (req, res) => {
+            res.render("selector", {
+                user: req.userData,
+                translate: req.translate,
+                locale: req.locale,
+                currentURL: `${req.client.config.dashboard.baseURL}/${req.originalUrl}`,
+                hasJoinedSupport: req.hasJoinedSupport,
+                discord: Constants.Links.SERVER
+            });
+        });
+    }
+};
+
+module.exports.name = '/selector';
