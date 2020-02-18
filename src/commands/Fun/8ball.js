51eadce5cd2706e4785043010967541f24aeb03b
Androz2091 <androz2091@gmail.com>
2020-02-18 14:55:12 +0100

    feat: upgrade 8ball command

---
+++ src/commands/Fun/8ball.js
@@ -0,0 +1,25 @@
+const Command = require("../../base/Command"),
+    Constants = require("../../utility/Constants");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: ["eightball", "8"],
+                permission: Constants.PermissionsLevels.SERVER_MEMBER
+            },
+            ...args
+        );
+    }
+
+    async execute(message, args) {
+        if (!args[0] || !message.content.endsWith("?")) {
+            return message.sendT("fun/8ball:ERR_QUESTION", null, "error");
+        }
+
+        const answerNO = parseInt(Math.floor(Math.random() * 10), 10);
+        const answer = message.translate(`fun/8ball:RESPONSE_${answerNO + 1}`);
+
+        message.channel.send(`${message.author.username}, ${answer}`);
+    }
+};
