c2586688b23b3eb3b5982e558cc455e31370ac4b
Androz2091 <androz2091@gmail.com>
2020-02-26 20:27:59 +0100

    feat: lmg command restored

---
+++ src/commands/Fun/lmg.js
@@ -0,0 +1,25 @@
+const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: ["lmgtfy"],
+                permission: Constants.PermissionsLevels.SERVER_MEMBER
+            },
+            ...args
+        );
+    }
+
+    async execute (message, args) {
+
+        const question = args.join(" ");
+        if(!question) return message.sendT("fun/lmg:MISSING", null, "error")
+        const encodedQuestion = question.replace(/[' '_]/g, "+");
+        await message.channel.send(`http://lmgtfy.com/?q=${encodedQuestion}`);
+        message.delete().catch(() => {});
+
+    }
+
+}
