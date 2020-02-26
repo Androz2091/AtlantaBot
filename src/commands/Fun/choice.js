38b9f6941f91cc442ed4da65e7bf085035d1b922
Androz2091 <androz2091@gmail.com>
2020-02-26 20:28:21 +0100

    feat: restore choice command

---
+++ src/commands/Fun/choice.js
@@ -0,0 +1,32 @@
+const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: ["random"],
+                permission: Constants.PermissionsLevels.SERVER_MEMBER
+            },
+            ...args
+        );
+    }
+
+    async execute (message, args) {
+
+        // Gets the answers by spliting on "/"
+        const answers = args.join(" ").split("/");
+        if(answers.length < 2) return message.sendT("fun/choice:MISSING", null, "error");
+        if(answers.some((answer) => !answer)) return message.sendT("fun/choice:EMPTY", null, "error")
+
+        let m = await message.sendT("fun/choice:PROGRESS", null, "loading");
+        
+        setTimeout(() => {
+            m.editT("fun/choice:DONE", null, "success");
+            const result = answers[parseInt(Math.floor(Math.random() * answers.length))];
+            message.channel.send("```"+result+"```");
+        }, 1500);
+
+    }
+
+}
