a60967c7d77cfb29655861c0dc25336b0a1c0182
Androz2091 <androz2091@gmail.com>
2020-02-26 21:06:04 +0100

    feat: restore lovecalc command

---
+++ src/commands/Fun/lovecalc.js
@@ -0,0 +1,42 @@
+const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants"),
+    Discord = require("discord.js"),
+    md5 = require("md5");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                permission: Constants.PermissionsLevels.SERVER_MEMBER
+            },
+            ...args
+        );
+    }
+
+    async execute(message) {
+
+        const firstMember = message.mentions.members.first();
+        const secondMember = message.mentions.members.filter((m) => m.id !== firstMember.id).first() || message.member;
+        if(!firstMember || !secondMember) return message.sendT("fun/lovecalc:MISSING", null, "error");
+
+        const members = [ firstMember, secondMember ].sort((a, b) => parseInt(a.id, 10) - parseInt(b.id, 10));
+        const hash = md5(`${members[0].id}${members[1].user.username}${members[0].user.username}${members[1].id}`);
+
+        const string = hash.split("").filter((e) => !isNaN(e)).join("");
+        const percent = parseInt(string.substr(0, 2), 10);
+        
+        let embed = new Discord.MessageEmbed()
+            .setAuthor("❤️ LoveCalc")
+            .setDescription(message.translate("fun/lovecalc:CONTENT", {
+                percent,
+                firstUsername: firstMember.user.username,
+                secondUsername: secondMember.user.username
+            }))
+            .setColor(this.client.config.embed.color)
+            .setFooter(this.client.config.embed.footer);
+        
+        message.channel.send(embed);
+        
+    }
+
+}
