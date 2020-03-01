4c99b36b971161dcdabaddaa4e8b06bb5b63c48e
Androz2091 <androz2091@gmail.com>
2020-03-01 10:53:28 +0100

    feat: add emoji prefix

--- src/utility/Extenders.js
+++ src/utility/Extenders.js
@@ -45,16 +45,16 @@ Message.prototype.success = function(key, args, edit = false, embed = false) {
 
 
 // Translate and send the message
-Message.prototype.success = function(key, args, edit = false, embed = false) {
+Message.prototype.sendT = function(key, args, edit = false, embed = false, emoji = null) {
+    const prefix = emoji ? `${Constants.Emojis[emoji.toUpperCase()]} | ` : "";
     if(embed && this.channel.permissionsFor(this.guild.me).has("EMBED_LINKS")){
         const embed = {
             color: Constants.Colors.DEFAULT,
-            description: this.translate(key, args)
+            description: prefix+this.translate(key, args)
         };
         return edit ? this.edit({ embed }) : this.channel.send({ embed });
      } else {
-        const updatedContent = `${Constants.Emojis.DEFAULT} | ${this.translate(key, args)}`;
-        return edit ? this.edit(updatedContent) : this.channel.send(updatedContent);
+        return edit ? this.edit(prefix+this.translate(key, args)) : this.channel.send(prefix+this.translate(key, args));
     }
 };
 
