25c2a03e9224683f1a3a9b459e04d2a4689d44af
Androz2091 <androz2091@gmail.com>
2020-03-03 18:36:33 +0100

    feat: restore delcommand command

---
+++ src/commands/Administration/delcommand.js
@@ -0,0 +1,37 @@
+const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: [],
+                permission: Constants.PermissionsLevels.SERVER_MODERATOR
+            },
+            ...args
+        );
+    }
+
+    async execute(message, args) {
+
+        const name = args[0];
+        if(!name){
+            return message.error("administration/delcommand:MISSING");
+        }
+
+        if(!message.guild.settings.customCommands.get(name)){
+            return message.error("administration/delcommand:UNKNOWN_COMMAND", {
+                prefix: message.guild.settings.prefix,
+                commandName: name
+            });
+        }
+        
+        await message.guild.settings.deleteCustomCommand(name);
+
+        message.success("administration/delcommand:SUCCESS", {
+            prefix: message.guild.settings.prefix,
+            commandName: name
+        });
+    }
+    
+}
