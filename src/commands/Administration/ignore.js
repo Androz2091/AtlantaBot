9fe2b4935b3ec346b15be18a5a0e54965b2f76f6
Androz2091 <androz2091@gmail.com>
2020-03-03 19:15:32 +0100

    feat: restore ignore command

---
+++ src/commands/Administration/ignore.js
@@ -0,0 +1,42 @@
+const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants"),
+    Resolvers = require("../../utility/Resolvers");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: [],
+                permission: Constants.PermissionsLevels.SERVER_MODERATOR
+            },
+            ...args
+        );
+    }
+
+    async execute(message, args) {
+        const channel = await Resolvers.resolveChannel({
+            message,
+            search: args.join(" "),
+            channelType: "text"
+        });
+        if (!channel) {
+            return message.error("administration/ignore:INVALID_CHANNEL");
+        }
+
+        const isIgnored = message.guild.settings.ignoredChannels.includes(
+            channel.id
+        );
+
+        if (isIgnored) {
+            await message.guild.settings.deleteIgnoredChannel(channel.id);
+            return message.success("administration/ignore:SUCCESS_ENABLED", {
+                channel: channel.toString()
+            });
+        } else {
+            await message.guild.settings.addIgnoredChannel(channel.id);
+            return message.success("administration/ignore:SUCCESS_DISABLED", {
+                channel: channel.toString()
+            });
+        }
+    }
+};
