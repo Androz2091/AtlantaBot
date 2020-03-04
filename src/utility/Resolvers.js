7e516437d516626b284f52b566a65911088284c2
Androz2091 <androz2091@gmail.com>
2020-03-04 21:26:59 +0100

    feat: add resolveRole resolver

--- src/utility/Resolvers.js
+++ src/utility/Resolvers.js
@@ -29,6 +29,38 @@ const resolveChannel = async ({ message, search, channelType }) => {
     return;
 };
 
+const resolveRole = async ({ message, search }) => {
+    const contentToCheck = search || message.content;
+    if (!contentToCheck || typeof contentToCheck !== "string") return;
+    // Try by parsing the search
+    if (contentToCheck.match(/^<@&([0-9]{18})>/)) {
+        const [, roleID] = contentToCheck.match(/^<@&([0-9]{18})>/);
+        const roleFound = message.guild.roles.cache.get(roleID);
+        if (roleFound)
+            return roleFound;
+    }
+    // Try with ID
+    if (message.guild.roles.cache.has(search)) {
+        const roleFound = message.guild.roles.cache.get(search);
+        if (roleFound)
+            return roleFound;
+    }
+    // Try with name with @
+    if (
+        message.guild.roles.cache.some(
+            role => `@${role.name}` === search || role.name === search
+        )
+    ) {
+        const roleFound = message.guild.roles.cache.find(
+            role => `@${role.name}` === search || role.name === search
+        );
+        if (roleFound)
+            return roleFound;
+    }
+    return;
+};
+
 module.exports = {
-    resolveChannel
+    resolveChannel,
+    resolveRole
 };
