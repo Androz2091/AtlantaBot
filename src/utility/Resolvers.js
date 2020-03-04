d19b261d5bd4e5f2ff18382c073ebece6d26725e
Androz2091 <androz2091@gmail.com>
2020-03-04 14:54:48 +0100

    fix(resolvers): remove unused var

--- src/utility/Resolvers.js
+++ src/utility/Resolvers.js
@@ -3,7 +3,7 @@ const resolveChannel = async ({ message, search, channelType }) => {
     if (!contentToCheck || typeof contentToCheck !== "string") return;
     // Try by parsing the search
     if (contentToCheck.match(/^<#([0-9]{18})>/)) {
-        const [_, channelID] = contentToCheck.match(/^<#([0-9]{18})>/);
+        const [, channelID] = contentToCheck.match(/^<#([0-9]{18})>/);
         const channelFound = message.guild.channels.cache.get(channelID);
         if (channelFound && channelType && channelFound.type === channelType)
             return channelFound;
