9b53eab82cbb52ce5bdd9254c59a84e103d9f20d
Androz2091 <androz2091@gmail.com>
2020-03-04 14:55:02 +0100

    feat: restore fortnite shop command

---
+++ src/commands/Administration/setfortniteshop.js
@@ -0,0 +1,54 @@
+﻿const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants"),
+    Resolvers = require("../../utility/Resolvers");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: [],
+                permission: Constants.PermissionsLevels.SERVER_MODERATOR
+            },
+            ...args
+        );
+    }
+
+    async execute(message, args) {
+        if (
+            !this.client.config.apiKeys.fortniteFNBR ||
+            this.client.config.apiKeys.fortniteFNBR.length === ""
+        ) {
+            return message.error("misc:COMMAND_DISABLED");
+        }
+
+        const currentFortniteShopChannel =
+            message.guild.settings.specialChannels.fortniteshop;
+        const sentChannel = await Resolvers.resolveChannel({
+            message,
+            search: args.join(" "),
+            channelType: "text"
+        });
+
+        if (!sentChannel && currentFortniteShopChannel) {
+            await message.guild.settings.setSpecialChannel(
+                "fortniteshop",
+                null
+            );
+            return message.success(
+                "administration/setfortniteshop:SUCCESS_DISABLED"
+            );
+        } else {
+            const channel = sentChannel || message.channel;
+            await message.guild.settings.setSpecialChannel(
+                "fortniteshop",
+                channel.id
+            );
+            return message.success(
+                "administration/setfortniteshop:SUCCESS_ENABLED",
+                {
+                    channel: channel.toString()
+                }
+            );
+        }
+    }
+};
