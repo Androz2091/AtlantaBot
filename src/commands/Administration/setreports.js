09ed10b247f18130fab19b3f51c00fc1f9647b28
Androz2091 <androz2091@gmail.com>
2020-03-04 15:57:15 +0100

    feat: restore setreports command

---
+++ src/commands/Administration/setreports.js
@@ -0,0 +1,44 @@
+const Command = require("../../structures/Command"),
+    Constants = require("../../utility/Constants"),
+    Resolvers = require("../../utility/Resolvers");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(
+            {
+                aliases: [],
+                permission: Constants.PermissionsLevels.SERVER_MODERATOR
+            },
+            ...args
+        );
+    }
+
+    async execute(message, args) {
+        const currentSuggestionsChannel =
+            message.guild.settings.specialChannels.reports;
+        const sentChannel = await Resolvers.resolveChannel({
+            message,
+            search: args.join(" "),
+            channelType: "text"
+        });
+
+        if (!sentChannel && currentSuggestionsChannel) {
+            await message.guild.settings.setSpecialChannel("reports", null);
+            return message.success(
+                "administration/setreports:SUCCESS_DISABLED"
+            );
+        } else {
+            const channel = sentChannel || message.channel;
+            await message.guild.settings.setSpecialChannel(
+                "reports",
+                channel.id
+            );
+            return message.success(
+                "administration/setreports:SUCCESS_ENABLED",
+                {
+                    channel: channel.toString()
+                }
+            );
+        }
+    }
+};
