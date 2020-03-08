ae8104f180edb12833e47bff9d71d6558b9fe35a
Androz2091 <androz2091@gmail.com>
2020-03-08 18:27:38 +0100

    feat(member): add cooldowns

--- src/models/Member.js
+++ src/models/Member.js
@@ -3,15 +3,16 @@ const { Collection } = require("discord.js");
 module.exports = class Member {
     constructor(userID, guildID, data = {}, handler) {
         this.id = userID;
+        this.guildID = guildID
         this.handler = handler;
         this.inserted = data !== {};
         this.data = data;
         // Whether the member is fetched
         this.fetched = false;
-        // Member guild id
-        this.guildID = guildID;
         // Member exp
         this.exp = data.exp || 0;
+        // Member work streak
+        this.workStreak = data.work_streak || 0;
     }
 
     async fetch() {
@@ -19,48 +20,93 @@ module.exports = class Member {
         this.transactions = [];
         await this.fetchTransactions();
         this.calcMoney();
+        this.cooldowns = {};
+        await this.fetchCooldowns();
         this.fetched = true;
+    }
+
+    async setWorkStreak(value){
+        await this.handler.query(`
+            UPDATE members
+            SET work_streak = ${value}
+            WHERE user_id = '${this.id}'
+            AND guild_id = '${this.guildID}';
+        `);
+        this.workStreak = value;
+        return this;
+    }
+
+    async addCooldown(commandName, endDate){
+        const alreadyCreated = this.cooldowns.hasOwnProperty(commandName);
+        if (alreadyCreated) {
+            await this.handler.query(`
+                UPDATE member_cmd_cooldown
+                SET end_date = '${endDate.toUTCString()}'
+                WHERE cmd_name = '${commandName}'
+                AND user_id = '${this.id}'
+                AND guild_id = '${this.guildID}';
+            `);
+        } else {
+            await this.handler.query(`
+                INSERT INTO member_cmd_cooldown
+                (user_id, guild_id, cmd_name, end_date) VALUES
+                ('${this.id}', '${this.guildID}', '${commandName}', '${endDate.toUTCString()}');
+            `);
+        }
+        this.cooldowns[commandName] = new Date(endDate).getTime();
+        return this;
+    }
+
+    async fetchCooldowns() {
+        const { rows } = await this.handler.query(`
+            SELECT * FROM member_cmd_cooldown
+            WHERE user_id = '${this.id}'
+            AND guild_id = '${this.guildID}';
+        `);
+        rows.forEach(cooldownData => {
+            this.cooldowns[cooldownData.cmd_name] = new Date(cooldownData.end_date).getTime();
+        });
+        return this;
+    }
+
+    async createTransaction({ action, date = new Date(), debit = 0, credit = 0, relatedMemberID }) {
+        await this.handler.query(`
+            INSERT INTO member_transactions
+            (user_id, guild_id, transac_action, transac_date, transac_debit, transac_credit  ${relatedMemberID ? ", transac_related_member_id" : ""}) VALUES
+            ('${this.id}', '${this.guildID}', '${action}', '${date.toUTCString()}', ${debit}, ${credit} ${relatedMemberID ? `, '${relatedMemberID}'` : ""});
+        `);
+        
+        this.transactions.push({
+            action,
+            date,
+            debit,
+            credit,
+            relatedMemberID
+        });
+        this.calcMoney();
+        return this;
     }
 
     async fetchTransactions() {
         const { rows } = await this.handler.query(`
-            SELECT * FROM members_transactions
+            SELECT * FROM member_transactions
             WHERE user_id = '${this.id}'
             AND guild_id = '${this.guildID}';
         `);
-        rows.sort((a, b) => a.trans_date - b.trans_date).forEach(transactionData => {
+        rows.sort((a, b) => a.transac_date - b.transac_date).forEach(transactionData => {
             this.transactions.push({
-                action: transactionData.trans_action,
-                date: transactionData.trans_date,
-                money: transactionData.trans_value,
-                sender: transactionData.trans_sender
+                action: transactionData.transac_action,
+                date: transactionData.transac_date,
+                debit: transactionData.transac_debit,
+                credit: transactionData.transac_credit,
+                relatedMemberID: transactionData.transac_related_member_id
             });
         });
     }
 
     calcMoney() {
-        let cash = 0,
-        bank = 0;
-        this.transactions.forEach((transaction) => {
-            switch(transaction.action){
-                case "send":
-                    cash -= t.money;
-                    break;
-                case "receive":
-                    cash += t.money;
-                    break;
-                case "deposit":
-                    cash -= t.money;
-                    bank += t.money;
-                    break;
-                case "withdraw":
-                    bank -= t.money;
-                    cash += t.money;
-                    break;
-            }
-        });
-        this.cash = cash;
-        this.bank = bank;
+        this.cash = (this.transactions.map((t) => t.debit + t.credit).length > 0 ? this.transactions.map((t) => t.debit + t.credit) : [0]).reduce((p, c) => p + c);
+        this.bank = (this.transactions.filter((t) => t.action === "bank_transfer").map((t) => t.debit - t.credit).length ? this.transactions.filter((t) => t.action === "bank_transfer").map((t) => t.debit - t.credit) : [0]).reduce((p, c) => p + c);
     }
 
     // Insert the member in the db if it doesn't exist
