5bbab540f320a128f5493c1219a283cdc559fa88
Androz2091 <androz2091@gmail.com>
2020-05-06 15:09:44 +0200

    :alien: Add i18n for fml command

--- commands/Fun/fml.js
+++ commands/Fun/fml.js
@@ -1,16 +1,11 @@
 const Command = require("../../base/Command.js"),
-Discord = require("discord.js"),
-cheerio = require("cheerio"),
-fetch = require("node-fetch");
+Discord = require("discord.js");
 
 class Fml extends Command {
 
     constructor (client) {
         super(client, {
             name: "fml",
-            description: (language) => language.get("FML_DESCRIPTION"),
-            usage: (language) => language.get("FML_USAGE"),
-            examples: (language) => language.get("FML_EXAMPLES"),
             dirname: __dirname,
             enabled: true,
             guildOnly: false,
@@ -25,25 +20,19 @@ class Fml extends Command {
 
     async run (message, args, data) {
 
-        fetch(message.language.get("FML_URL"))
-        .then(res => {
-            if(res.ok){
-            res.text().then(async body => {
-                let $ = cheerio.load(body),
-                content = await $(".article-contents").children("a").first().text().replace(/[\n]/gi, " ").replace(" -", "-"),
-                credits = await $(".panel.panel-classic").children("div").first().text(),
-                embed = new Discord.MessageEmbed()
-                .setAuthor(message.language.get("FML_TITLE"), this.client.user.displayAvatarURL())
-                .setDescription(content)
-                .setFooter(credits)
-                .setColor(data.config.embed.color)
-    
-                message.channel.send(embed);
-            })
-            } else {
-                return message.channel.send(message.language.get("FML_ERROR"));
-            }
-        }).catch()
+        if (!this.client.config.apiKeys.blagueXYZ)
+            return message.error("misc:COMMAND_DISABLED");
+
+        const fml = await this.client.joker.randomVDM(
+            message.guild.settings.language.substr(0, 2)
+        );
+
+        const embed = new Discord.MessageEmbed()
+            .setDescription(fml.content)
+            .setFooter(message.translate("fun/fml:FOOTER"))
+            .setColor(this.client.config.embed.color);
+
+        message.channel.send(embed);
 
     }
 
