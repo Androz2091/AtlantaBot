ddec5f24a9fe98d5a1afb756ff6c7f074e14fae1
Androz2091 <androz2091@gmail.com>
2020-05-09 21:52:40 +0200

    :bug: Remove cache prop

--- helpers/resolvers.js
+++ helpers/resolvers.js
@@ -4,23 +4,23 @@ const resolveChannel = async ({ message, search, channelType }) => {
     // Try by parsing the search
     if (contentToCheck.match(/^<#([0-9]{18})>/)) {
         const [, channelID] = contentToCheck.match(/^<#([0-9]{18})>/);
-        const channelFound = message.guild.channels.cache.get(channelID);
+        const channelFound = message.guild.channels.get(channelID);
         if (channelFound && channelType && channelFound.type === channelType)
             return channelFound;
     }
     // Try with ID
-    if (message.guild.channels.cache.has(search)) {
-        const channelFound = message.guild.channels.cache.get(search);
+    if (message.guild.channels.has(search)) {
+        const channelFound = message.guild.channels.get(search);
         if (channelFound && channelType && channelFound.type === channelType)
             return channelFound;
     }
     // Try with name with #
     if (
-        message.guild.channels.cache.some(
+        message.guild.channels.some(
             channel => `#${channel.name}` === search || channel.name === search
         )
     ) {
-        const channelFound = message.guild.channels.cache.find(
+        const channelFound = message.guild.channels.find(
             channel => `#${channel.name}` === search || channel.name === search
         );
         if (channelFound && channelType && channelFound.type === channelType)
@@ -50,11 +50,11 @@ const resolveMember = async ({ message, search, useMessageContent = true }) => {
         query: search
     });
     if (
-        message.guild.members.cache.some(
+        message.guild.members.some(
             member => member.user.tag === search || member.user.username === search
         )
     ) {
-        const memberFound = message.guild.members.cache.find(
+        const memberFound = message.guild.members.find(
             member => member.user.tag === search || member.user.username === search
         );
         if (memberFound)
@@ -69,23 +69,23 @@ const resolveRole = async ({ message, search }) => {
     // Try by parsing the search
     if (contentToCheck.match(/^<@&([0-9]{18})>/)) {
         const [, roleID] = contentToCheck.match(/^<@&([0-9]{18})>/);
-        const roleFound = message.guild.roles.cache.get(roleID);
+        const roleFound = message.guild.roles.get(roleID);
         if (roleFound)
             return roleFound;
     }
     // Try with ID
-    if (message.guild.roles.cache.has(search)) {
-        const roleFound = message.guild.roles.cache.get(search);
+    if (message.guild.roles.has(search)) {
+        const roleFound = message.guild.roles.get(search);
         if (roleFound)
             return roleFound;
     }
     // Try with name with @
     if (
-        message.guild.roles.cache.some(
+        message.guild.roles.some(
             role => `@${role.name}` === search || role.name === search
         )
     ) {
-        const roleFound = message.guild.roles.cache.find(
+        const roleFound = message.guild.roles.find(
             role => `@${role.name}` === search || role.name === search
         );
         if (roleFound)
