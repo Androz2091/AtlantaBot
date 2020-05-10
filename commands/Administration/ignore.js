184ba911f3f4bf16aaed70d31b9f22e4b1a96114
Androz2091 <androz2091@gmail.com>
2020-05-10 21:10:07 +0200

    :alien: Add i18n for ignore command

--- commands/Administration/ignore.js
+++ commands/Administration/ignore.js
@@ -6,9 +6,6 @@ class Ignore extends Command {
     constructor (client) {
         super(client, {
             name: "ignore",
-            description: (language) => language.get("IGNORE_DESCRIPTION"),
-            usage: (language) => language.get("IGNORE_USAGE"),
-            examples: (language) => language.get("IGNORE_EXAMPLES"),
             dirname: __dirname,
             enabled: true,
             guildOnly: true,
@@ -25,7 +22,7 @@ class Ignore extends Command {
 
         let channel = message.mentions.channels.filter((ch) => ch.type === "text" && ch.guild.id === message.guild.id).first();
         if(!channel){
-            return message.channel.send(message.language.get("ERR_INVALID_CHANNEL"));
+            return message.error("misc:INVALID_CHANNEL");
         }
 
         let ignored = data.guild.ignoredChannels.includes(channel.id);
@@ -33,11 +30,15 @@ class Ignore extends Command {
         if(ignored){
             data.guild.ignoredChannels = data.guild.ignoredChannels.filter((ch) => ch !== channel.id);
             data.guild.save();
-            return message.channel.send(message.language.get("IGNORE_SUCCESS_DISABLED", channel));
+            return message.error("administration/ignore:ALLOWED", {
+                channel
+            });
         } else if(!ignored){
             data.guild.ignoredChannels.push(channel.id);
             data.guild.save();
-            return message.channel.send(message.language.get("IGNORE_SUCCESS_ENABLED", channel));
+            return message.error("administration/ignore:IGNORED", {
+                channel
+            });
         }
         
     }
