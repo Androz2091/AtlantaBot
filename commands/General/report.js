bcdf0c45353da8967d8ac0829cd280a9ec54f02e
Androz2091 <androz2091@gmail.com>
2020-05-29 16:15:03 +0200

    :alien: Add i18n for report command

--- commands/General/report.js
+++ commands/General/report.js
@@ -6,9 +6,6 @@ class Report extends Command {
     constructor (client) {
         super(client, {
             name: "report",
-            description: (language) => language.get("REPORT_DESCRIPTION"),
-            usage: (language) => language.get("REPORT_USAGE"),
-            examples: (language) => language.get("REPORT_EXAMPLES"),
             dirname: __dirname,
             enabled: true,
             guildOnly: true,
@@ -25,34 +22,32 @@ class Report extends Command {
 
         let repChannel = message.guild.channels.get(data.guild.plugins.reports);
         if(!repChannel){
-            return message.channel.send(message.language.get("REPORT_ERR_NO_CHANNEL"));
+            return message.error("general/report:MISSING_CHANNEL");
         }
 
         let member = await this.client.resolveMember(args[0], message.guild);
         if(!member){
-            return message.channel.send(message.language.get("ERR_INVALID_MEMBER"));
+            return message.error("general/report:MISSING_USER");
         }
 
         if(member.id === message.author.id){
-            return message.channel.send(message.language.get("REPORT_ERR_USER_YOURSELF"));
-        }
- 
-        if(member.user.bot){
-            return message.channel.send(message.language.get("ERR_BOT_USER"));
+            return message.error("general/report:INVALID_USER");
         }
 
         let rep = args.slice(1).join(" ");
         if(!rep){
-            return message.channel.send(message.language.get("REPORT_ERR_NO_REP"));
+            return message.error("general/report:MISSING_REASON");
         }
 
         let embed = new Discord.MessageEmbed()
-            .setAuthor(message.language.get("REPORT_TITLE", message.author), message.author.displayAvatarURL())
-            .addField(message.language.get("REPORT_HEADINGS")[0], message.author.tag, true)
-            .addField(message.language.get("REPORT_HEADINGS")[1], message.language.printDate(new Date(Date.now()), true), true)
+            .setAuthor(message.translate("general/report:TITLE", {
+                user: member.user.tag
+            }), message.author.displayAvatarURL())
+            .addField(message.translate("common:AUTHOR"), message.author.tag, true)
+            .addField(message.translate("common:DATE"), message.language.printDate(new Date(Date.now()), true), true)
             .addBlankField()
-            .addField(message.language.get("REPORT_HEADINGS")[2], "**"+rep+"**", true)
-            .addField(message.language.get("REPORT_HEADINGS")[3], `\`${member.user.tag}\` (${member.user.toString()})`, true)
+            .addField(message.translate("common:REASON"), "**"+rep+"**", true)
+            .addField(message.translate("common:USER"), `\`${member.user.tag}\` (${member.user.toString()})`, true)
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer);
 
@@ -64,7 +59,9 @@ class Report extends Command {
             await m.react(error);
         });
 
-        message.channel.send(message.language.get("REPORT_SUCCESS", repChannel));
+        message.success("general/report:SUCCESS", {
+            channel: repChannel.toString()
+        });
     }
 
 }
