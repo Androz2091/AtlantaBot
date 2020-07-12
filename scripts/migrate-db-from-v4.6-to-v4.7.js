eb84758ffba18c0d8d88fc846021af86dffb5e2e
Androz2091 <androz2091@gmail.com>
2020-07-12 12:43:16 +0200

    :hammer: Add v4.6 to v4.7 db migration script

---
+++ scripts/migrate-db-from-v4.6-to-v4.7.js
@@ -0,0 +1,36 @@
+const chalk = require("chalk");
+console.log(chalk.blue("Migrating database from v4.6.4 to v4.7.0..."));
+
+let MongoClient;
+try {
+	MongoClient = require("mongodb").MongoClient;
+} catch (e) {
+	console.log(chalk.red("Cannot find module mongodb. Please install it using \"npm install mongodb\" before executing migration scripts."));
+	process.exit(1);
+}
+
+const config = require("../config.js");
+const dbName = config.mongoDB.split("/").pop();
+const baseURL = config.mongoDB.substr(0, config.mongoDB.length - dbName.length);
+const client = new MongoClient(baseURL, {
+	useUnifiedTopology: true
+});
+client.connect().then(async () => {
+	console.log(chalk.green("Connected successfully to mongoDB database."));
+   
+	const db = client.db(dbName);
+	const guilds = db.collection("guilds");
+
+	const count = await guilds.countDocuments({ $or: [ { language: "english" }, { language: "french" } ] });
+	console.log(chalk.yellow(`${count} guilds need to be migrated. Migrating...`));
+
+	await guilds.updateMany({ language: "english" }, { $set: { language: "en-US"} });
+	await guilds.updateMany({ language: "french" }, { $set: { language: "fr-FR"} });
+    
+	console.log(chalk.green(`${count} guilds migrated.`));
+	console.log(chalk.blue("Database migrated from v4.6.4 to v4.7.0..."));
+	process.exit(0);
+}).catch(() => {
+	console.log(chalk.red("Couldn't connect to mongoDB database..."));
+	process.exit(1);
+});
