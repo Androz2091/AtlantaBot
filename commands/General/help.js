e3e0f615e90693eb1360ca5ecfaf373c9ea42208
Androz2091 <androz2091@gmail.com>
2020-07-12 11:06:57 +0200

    :bug: Fix examples and usage on help page

--- commands/General/help.js
+++ commands/General/help.js
@@ -37,13 +37,13 @@ class Help extends Command {
 			}
 
 			const description = message.translate(`${cmd.help.category.toLowerCase()}/${cmd.help.name}:DESCRIPTION`);
-			const usage = message.translate(`${cmd.help.category.toLowerCase()}/${cmd.name}:USAGE`, {
+			const usage = message.translate(`${cmd.help.category.toLowerCase()}/${cmd.help.name}:USAGE`, {
 				prefix: message.guild
 					? data.guild.prefix
 					: ""
 			}
 			);
-			const examples = message.translate(`${cmd.help.category.toLowerCase()}/${cmd.name}:EXAMPLES`, {
+			const examples = message.translate(`${cmd.help.category.toLowerCase()}/${cmd.help.name}:EXAMPLES`, {
 				prefix: message.guild
 					? data.guild.prefix
 					: ""
