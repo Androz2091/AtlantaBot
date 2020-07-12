f0c693f735920e60031300f11fcff7d1410b8078
Androz2091 <androz2091@gmail.com>
2020-07-12 16:08:48 +0200

    :bug: Fix report command

--- commands/General/report.js
+++ commands/General/report.js
@@ -44,8 +44,7 @@ class Report extends Command {
 				user: member.user.tag
 			}), message.author.displayAvatarURL())
 			.addField(message.translate("common:AUTHOR"), message.author.tag, true)
-			.addField(message.translate("common:DATE"), message.printDate(new Date(Date.now()), true), true)
-			.addBlankField()
+			.addField(message.translate("common:DATE"), message.printDate(new Date(Date.now())), true)
 			.addField(message.translate("common:REASON"), "**"+rep+"**", true)
 			.addField(message.translate("common:USER"), `\`${member.user.tag}\` (${member.user.toString()})`, true)
 			.setColor(data.config.embed.color)
