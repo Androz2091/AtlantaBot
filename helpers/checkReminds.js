e0c351c1d6828334fd5f21073418b5c44d5227e2
Androz2091 <androz2091@gmail.com>
2020-07-13 21:48:09 +0200

    :bug: Fix reminds check

--- helpers/checkReminds.js
+++ helpers/checkReminds.js
@@ -19,22 +19,20 @@ module.exports = {
 			client.databaseCache.usersReminds.forEach(async (user) => {
 				const dUser = client.users.cache.get(user.id);
 				if(dUser){
-					const reminds = user.reminds || [];
-					if(reminds.length > 0){
-						const mustSent = reminds.filter((r) => r.sendAt < dateNow);
-						if(mustSent.length > 0){
-							mustSent.forEach((r) => {
-								const embed = new Discord.MessageEmbed()
-									.setAuthor(client.translate("general/remindme:TITLE"))
-									.addField(client.translate("common:CREATION"), client.translate("general/remindme:CREATED", {
-										time: client.convertTime(r.createdAt, "from")
-									}))
-									.addField(client.translate("common:MESSAGE"), r.message)
-									.setColor(client.config.embed.color)
-									.setFooter(client.config.embed.footer);
-								dUser.send(embed);
-							});
-						}
+					const reminds = user.reminds;
+					const mustSent = reminds.filter((r) => r.sendAt < dateNow);
+					if(mustSent.length > 0){
+						mustSent.forEach((r) => {
+							const embed = new Discord.MessageEmbed()
+								.setAuthor(client.translate("general/remindme:TITLE"))
+								.addField(client.translate("common:CREATION"), client.translate("general/remindme:CREATED", {
+									time: client.convertTime(r.createdAt, "from")
+								}))
+								.addField(client.translate("common:MESSAGE"), r.message)
+								.setColor(client.config.embed.color)
+								.setFooter(client.config.embed.footer);
+							dUser.send(embed);
+						});
 						user.reminds = user.reminds.filter((r) => r.sendAt >= dateNow);
 						user.save();
 						if(user.reminds.length === 0){
