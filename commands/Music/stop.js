9e1967eec78b1e847e6777558be1aed046cd19c1
Sh4yn <me@shaynlink.fr>
2020-07-15 10:22:48 +0200

    Use filter user bot insteadof filter client id (#242)

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -31,7 +31,7 @@ class Stop extends Command {
 			return message.error("music/play:NOT_PLAYING");
 		}
 
-		const members = voice.members.filter((m) => m.id !== this.client.user.id);
+		const members = voice.members.filter((m) => !m.user.bot);
 
 		const embed = new Discord.MessageEmbed()
 			.setAuthor(message.translate("music/stop:DESCRIPTION"))
