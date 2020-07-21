5cfb179f35173607d4c5dc1b0104f252263ed362
shaynlink <me@shaynlink.fr>
2020-07-21 04:09:13 +0200

    better use of RAM

--- helpers/checkReminds.js
+++ helpers/checkReminds.js
@@ -7,14 +7,16 @@ module.exports = {
      * Starts checking...
      * @param {object} client The Discord Client instance
      */
-	init(client){
-		client.usersData.find({ reminds: { $gt : [] } }).then((users) => {
+	async init(client){
+		await client.usersData.find({ reminds: { $gt : [] } }).then((users) => {
 			for(const user of users){
 				if(!client.users.cache.has(user.id)) client.users.fetch(user.id);
 				client.databaseCache.usersReminds.set(user.id, user);
 			}
+
+			return; // for Complete await
 		});
-		setInterval(async function(){
+		/*setInterval(async function(){
 			const dateNow = Date.now();
 			client.databaseCache.usersReminds.forEach(async (user) => {
 				const dUser = client.users.cache.get(user.id);
@@ -41,7 +43,48 @@ module.exports = {
 					}
 				}
 			});
-		}, 1000);
+		}, 1000);*/
+
+		/**
+		 * @prefer use setTimeout for to use proportionally RAM
+		 * why don't use setInterval, because instead of use correctly RAM, he makes use of spikes.
+		 * And aslo it can redo a loop without the other being finish.
+		 */
+		client.databaseCache.usersReminds.forEach(async (user) => {
+			const dUser = client.users.cache.get(user.id);
+			if (dUser) {
+				user.reminds.forEach(async (remind) => {
+					if (remind.sendAt > Date.now()) {
+						this.sendRemind(client, user, dUser, remind);
+					} else {
+						setTimeout(() => {
+							this.sendRemind(client, user, dUser, remind);
+						}, remind.sendAt - Date.now());
+					}
+				});
+			}
+		});
+	},
+
+	async sendRemind(client, user, dUser, remind) {
+		// RATE-LIMIT
+		await new Promise((resolve) => setTimeout(resolve, 500));
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(client.translate("general/remindme:TITLE"))
+			.addField(client.translate("common:CREATION"), client.translate("general/remindme:CREATED", {
+				time: client.convertTime(remind.createdAt, "from")
+			}))
+			.addField(client.translate("common:MESSAGE"), remind.message)
+			.setColor(client.config.embed.color)
+			.setFooter(client.config.embed.footer);
+		dUser.send(embed);
+
+		const pos = user.reminds.findIndex((r) => r.createdAt === remind.createdAt);
+		user.reminds.splice(pos, 1);
+		user.save();
+		if (user.reminds.length === 0) {
+			client.databaseCache.usersReminds.delete(user.id);
+		}
 	}
 
 };
