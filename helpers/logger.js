e5d252cc6f79f0bbdde22b7d08547dfdb4db7eba
Androz2091 <androz2091@gmail.com>
2020-08-14 09:18:47 +0200

    :art: Format logger

--- helpers/logger.js
+++ helpers/logger.js
@@ -25,26 +25,26 @@ module.exports = class Logger {
 	static log (content, type = "log") {
 		const date = `[${format(new Date(Date.now()))}]:`;
 		switch (type) {
-		// Check the message type and then print him in the console
-		case "log": {
-			return console.log(`${date} ${bgBlue(type.toUpperCase())} ${content} `);
-		}
-		case "warn": {
-			return console.log(`${date} ${black.bgYellow(type.toUpperCase())} ${content} `);
-		}
-		case "error": {
-			return console.log(`${date} ${black.bgRed(type.toUpperCase())} ${content} `);
-		}
-		case "debug": {
-			return console.log(`${date} ${green(type.toUpperCase())} ${content} `);
-		}
-		case "cmd": {
-			return console.log(`${date} ${black.bgWhite(type.toUpperCase())} ${content}`);
-		}
-		case "ready": {
-			return console.log(`${date} ${black.bgGreen(type.toUpperCase())} ${content}`);
-		} 
-		default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+			// Check the message type and then print him in the console
+			case "log": {
+				return console.log(`${date} ${bgBlue(type.toUpperCase())} ${content} `);
+			}
+			case "warn": {
+				return console.log(`${date} ${black.bgYellow(type.toUpperCase())} ${content} `);
+			}
+			case "error": {
+				return console.log(`${date} ${black.bgRed(type.toUpperCase())} ${content} `);
+			}
+			case "debug": {
+				return console.log(`${date} ${green(type.toUpperCase())} ${content} `);
+			}
+			case "cmd": {
+				return console.log(`${date} ${black.bgWhite(type.toUpperCase())} ${content}`);
+			}
+			case "ready": {
+				return console.log(`${date} ${black.bgGreen(type.toUpperCase())} ${content}`);
+			} 
+			default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
 		}
 	}
 };
