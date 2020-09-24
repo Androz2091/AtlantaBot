27a8213c81d1b5afcfc00f4547ee17d75345ad01
Androz2091 <androz2091@gmail.com>
2020-09-24 18:15:38 +0200

    :bug: Add error message when the sentry key is invalid

--- atlanta.js
+++ atlanta.js
@@ -4,11 +4,17 @@ const Sentry = require("@sentry/node"),
 	util = require("util"),
 	fs = require("fs"),
 	readdir = util.promisify(fs.readdir),
-	mongoose = require("mongoose");
+	mongoose = require("mongoose"),
+	chalk = require("chalk");
 
 const config = require("./config");
 if(config.apiKeys.sentryDSN){
-	Sentry.init({ dsn: config.apiKeys.sentryDSN });
+	try {
+		Sentry.init({ dsn: config.apiKeys.sentryDSN });
+	} catch (e) {
+		console.log(e);
+		console.log(chalk.yellow("Looks like your Sentry DSN key is invalid. If you do not intend to use Sentry, please remove the key from the configuration file."));
+	}
 }
 
 // Load Atlanta class
