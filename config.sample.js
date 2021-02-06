b4b4ecb82a7643fe4766dab68e60aa1fdd2cb127
Bhavishya Maheshwari <77373408+BhavishyaMaheshwari@users.noreply.github.com>
2021-02-06 15:17:33 +0530

    .env

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,6 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "XXXXXXXXXXX",
+	token: process.env.TOKEN,
 	/* For the support server */
 	support: {
 		id: "XXXXXXXXXXX", // The ID of the support server
