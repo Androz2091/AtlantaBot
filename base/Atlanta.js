e9675521606e4f8497343da5cff81007d3cc6fca
Muhit Raihan <36473811+muhitrhn@users.noreply.github.com>
2021-05-18 12:00:10 +0600

    :sparkles: Specified proper intents to fix play command

--- base/Atlanta.js
+++ base/Atlanta.js
@@ -25,6 +25,7 @@ class Atlanta extends Client {
 				Intents.FLAGS.GUILD_MEMBERS,
 				Intents.FLAGS.GUILD_MESSAGES,
 				Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+				Intents.FLAGS.GUILD_VOICE_STATES,
 				Intents.FLAGS.DIRECT_MESSAGES
 			],
 			allowedMentions: {
