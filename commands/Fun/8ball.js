2dfe8089c1c17bdf2763738672cf230f2c7b631c
Yan-Jobs <yan@yanjobs.me>
2021-05-21 19:30:53 +0200

    :tada: Fix bot wickedness problem !
    Now he respond **YES** when we asking if we are beautiful.

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -1,34 +1,36 @@
 const Command = require("../../base/Command.js");
 
 class Eightball extends Command {
+  constructor(client) {
+    super(client, {
+      name: "8ball",
+      dirname: __dirname,
+      enabled: true,
+      guildOnly: false,
+      aliases: ["eight-ball", "eightball"],
+      memberPermissions: [],
+      botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+      nsfw: false,
+      ownerOnly: false,
+      cooldown: 3000,
+    });
+  }
 
-	constructor (client) {
-		super(client, {
-			name: "8ball",
-			dirname: __dirname,
-			enabled: true,
-			guildOnly: false,
-			aliases: [ "eight-ball", "eightball" ],
-			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-			nsfw: false,
-			ownerOnly: false,
-			cooldown: 3000
-		});
-	}
+  async run(message, args) {
+    if (!args[0] || !message.content.endsWith("?")) {
+      return message.error("fun/8ball:ERR_QUESTION");
+    };
+    if (message.content.toLowerCase() == "suis je beau ?" || "am i beautiful ?" || "suis-je beau ?") {
+      const answer = message.translate(`fun/8ball:RESPONSE_2`);
 
-	async run (message, args) {
-        
-		if (!args[0] || !message.content.endsWith("?")) {
-			return message.error("fun/8ball:ERR_QUESTION");
-		}
+      message.channel.send(`${message.author.username}, ${answer}`);
+    } else {
+      const answerNO = parseInt(Math.floor(Math.random() * 10), 10);
+      const answer = message.translate(`fun/8ball:RESPONSE_${answerNO + 1}`);
 
-		const answerNO = parseInt(Math.floor(Math.random() * 10), 10);
-		const answer = message.translate(`fun/8ball:RESPONSE_${answerNO + 1}`);
-
-		message.channel.send(`${message.author.username}, ${answer}`);
-	}
-
-}
+      message.channel.send(`${message.author.username}, ${answer}`);
+    }
+  }
+};
 
 module.exports = Eightball;
