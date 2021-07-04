48e5727d0981d57766f6fef3eb820b4f6bf66950
squarfiuz <70509576+squarfiuznath@users.noreply.github.com>
2021-07-04 10:51:41 +0200

    Update CheckAuth.js

--- dashboard/auth/CheckAuth.js
+++ dashboard/auth/CheckAuth.js
@@ -1,10 +1,9 @@
 module.exports = async (req, res, next) => {
-	if(req.session.user){
-		return next();
-	} else {
+	if(!req.session.user){
 		const redirectURL = ((req.originalUrl.includes("login") || req.originalUrl === "/") ? "/selector" : req.originalUrl);
 		const state = Math.random().toString(36).substring(5);
 		req.client.states[state] = redirectURL;
 		return res.redirect(`/api/login?state=${state}`);
 	}
+	return next();
 };
