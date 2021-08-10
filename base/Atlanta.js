e44a7727eab3f693b53af815b5b4424eee1db505
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2021-08-10 19:44:36 +0700

    Channel Partial
    enables those dms command

--- base/Atlanta.js
+++ base/Atlanta.js
@@ -30,7 +30,10 @@ class Atlanta extends Client {
 			],
 			allowedMentions: {
 				parse: ["users"]
-			}
+			},
+			partials: [
+				"CHANNEL"
+			],
 		});
 		this.config = require("../config"); // Load the config file
 		this.customEmojis = require("../emojis.json"); // load the bot's emojis
