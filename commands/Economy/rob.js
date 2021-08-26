41646df3c250ecde9a269b6f47df70d1a4e09ccf
Androz2091 <androz2091@gmail.com>
2021-08-26 21:47:28 +0200

    :recycle: Rewrite rob command

--- commands/Economy/rob.js
+++ commands/Economy/rob.js
@@ -5,65 +5,87 @@ module.exports = class extends Command {
 	constructor (client) {
 		super(client, {
 			name: "rob",
-			dirname: __dirname,
+
+			options: [
+				{
+					name: "user",
+					type: "USER"
+				},
+				{
+					name: "amount",
+					type: "INTEGER"
+				}
+			],
+
 			enabled: true,
 			guildOnly: true,
-			
 			memberPermissions: [],
 			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
 			nsfw: false,
 			ownerOnly: false,
-			cooldown: 1000
+
+			dirname: __dirname
 		});
 	}
 
 	async run (interaction, translate, data) {
 
-		const member = await this.client.resolveMember(args[0], message.guild);
-		if(!member){
+		const user = interaction.options.getUser("user");
+		if(!user){
 			return interaction.reply({
 				content: translate("economy/rob:MISSING_MEMBER"),
 				ephemeral: true
 			});
 		}
 
-		if(member.id === interaction.user.id){
+		if(user.id === interaction.user.id){
 			return interaction.reply({
 				content: translate("economy/rob:YOURSELF"),
 				ephemeral: true
 			});
 		}
 
-		const memberData = await this.client.findOrCreateMember({ id: member.id, guildID: interaction.guild.id });
+		const memberData = await this.client.findOrCreateMember({ id: user.id, guildID: interaction.guild.id });
 		const isInCooldown = memberData.cooldowns.rob || 0;
 		if(isInCooldown){
 			if(isInCooldown > Date.now()){
-				return message.error("economy/rob:COOLDOWN", {
-					username: member.user.tag
+				return interaction.reply({
+					content: translate("economy/rob:COOLDOWN", {
+						username: user.tag
+					}),
+					ephemeral: true
 				});
 			}
 		}
 
-		let amountToRob = args[1];
+		let amountToRob = interaction.options.getInteger("amount");
 		if(!amountToRob || isNaN(amountToRob) || parseInt(amountToRob, 10) <= 0){
-			return message.error("economy/rob:MISSING_AMOUNT", {
-				username: member.user.username
+			return interaction.reply({
+				content: translate("economy/rob:MISSING_AMOUNT", {
+					username: user.username
+				}),
+				ephemeral: true
 			});
 		}
 		amountToRob = Math.floor(parseInt(amountToRob, 10));
 
 		if(amountToRob > memberData.money){
-			return message.error("economy/rob:NOT_ENOUGH_MEMBER", {
-				username: member.user.username,
-				money: amountToRob
+			return interaction.reply({
+				content: translate("economy/rob:NOT_ENOUGH_MEMBER", {
+					username: user.username,
+					money: amountToRob
+				})
 			});
 		}
 
 		const potentiallyLose = Math.floor(amountToRob*1.5);
 		if(potentiallyLose > data.memberData.money){
-			return message.error("economy/rob:NOT_ENOUGH_AUTHOR", {
-				moneyMin: potentiallyLose,
-				moneyCurrent: data.memberData.money
+			return interaction.reply({
+				content: translate("economy/rob:NOT_ENOUGH_AUTHOR", {
+					moneyMin: potentiallyLose,
+					moneyCurrent: data.memberData.money
+				}),
+				ephemeral: true
 			});
 		}
 
@@ -75,9 +97,11 @@ module.exports = class extends Command {
 			memberData.markModified("cooldowns");
 			await memberData.save();
 			const randomNum = Math.floor(this.client.functions.randomNum(1, 3));
-			message.sendT("economy/rob:ROB_WON_"+randomNum, {
-				money: amountToRob,
-				username: member.user.username
+			interaction.reply({
+				content: translate("economy/rob:ROB_WON_"+randomNum, {
+					money: amountToRob,
+					username: user.username
+				})
 			});
 			data.memberData.money += amountToRob;
 			memberData.money -= amountToRob, 10;
@@ -86,10 +110,12 @@ module.exports = class extends Command {
 		} else {
 			const won = Math.floor(0.9*amountToRob);
 			const randomNum = Math.floor(this.client.functions.randomNum(1, 3));
-			message.sendT("economy/rob:ROB_LOSE_"+randomNum, {
-				fine: potentiallyLose,
-				offset: won,
-				username: member.user.username
+			interaction.reply({
+				content: translate("economy/rob:ROB_LOSE_"+randomNum, {
+					fine: potentiallyLose,
+					offset: won,
+					username: user.username
+				})
 			});
 			data.memberData.money -= potentiallyLose;
 			memberData.money += won;
