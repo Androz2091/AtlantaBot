26fcabeab77c9f4a753de50b0b97d3893614bed4
Derpinou <lightcompte@gmail.com>
2022-05-04 11:44:00 +0200

    :fire: Remove getPrefix() method

--- helpers/functions.js
+++ helpers/functions.js
@@ -5,31 +5,6 @@ languages.forEach((l) => {
 
 module.exports = {
 
-	/**
-     * Gets message prefix
-     * @param {object} message The Discord message
-     * @returns The prefix
-     */
-	getPrefix(message, data){
-		if(message.channel.type !== "dm"){
-			const prefixes = [
-				`<@!${message.client.user.id}> `,
-				`<@${message.client.user.id}> `,
-				message.client.user.username.toLowerCase(),
-				data.guild.prefix
-			];
-			let prefix = null;
-			prefixes.forEach((p) => {
-				if(message.content.startsWith(p) || message.content.toLowerCase().startsWith(p)){
-					prefix = p;
-				}
-			});
-			return prefix;
-		} else {
-			return true;
-		}
-	},
-
 	// This function return a valid link to the support server
 	async supportLink(client){
 		const guild = client.guilds.cache.get(client.config.support.id);
