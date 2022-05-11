1367d242973b8c1c6a22935164b074d730d25c1b
Derpinou <lightcompte@gmail.com>
2022-05-11 21:30:30 +0200

    :sparkles: Introducing commands permissions

--- base/Command.js
+++ base/Command.js
@@ -1,23 +1,34 @@
 const path = require("path");
+const {Permissions} = require("discord.js");
 
 module.exports = class Command {
 	constructor(client, {
 		name = null,
+		description = false,
 		dirname = false,
 		enabled = true,
 		guildOnly = false,
-		botPermissions = [],
 		memberPermissions = [],
+		botPermissions = [],
 		nsfw = false,
 		ownerOnly = false,
 		cooldown = 3000,
-		commandBody = {}
+		options = {}
 	})
 	{
-		const category = (dirname ? dirname.split(path.sep)[parseInt(dirname.split(path.sep).length-1, 10)] : "Other");
+		memberPermissions.forEach((p, i) => {
+			memberPermissions[i] = Permissions.FLAGS[p];
+		});
+		const bit = memberPermissions.reduce((a, b) => a | b, 0n).toString();
+		const category = dirname ? dirname.split(path.sep)[dirname.split(path.sep).length - 1] : "Other";
 		this.client = client;
-		this.conf = { enabled, guildOnly, memberPermissions, botPermissions, nsfw, ownerOnly, cooldown};
+		this.conf = { enabled, guildOnly, botPermissions, nsfw, ownerOnly, cooldown};
 		this.help = { name, category };
-		this.applicationCommandBody = commandBody;
+		this.applicationCommandBody = {name, description, options, dm_permission: !guildOnly};
+
+
+		if (bit !== "0") {
+			this.applicationCommandBody["default_member_permissions"] = bit;
+		}
 	}
 };
